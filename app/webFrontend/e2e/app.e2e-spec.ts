e4763a399011298077434130d05f242d19f3e486
Thomas Sauer <thomas@t-sauer.de>
2017-04-04 16:50:36 +0200

    fix example e2e test

--- app/webFrontend/e2e/app.e2e-spec.ts
+++ app/webFrontend/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('web-frontend App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to LearnIT');
   });
 });
