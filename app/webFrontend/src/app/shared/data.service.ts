7cb68b6b87a90fbceda732cd9bc6b7200878d25e
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-04-14 16:56:40 +0200

    First commit of edit user feature (#6), created user role component, added component to admin dashboard, created admin menu, created route for user role component, fixed tslint errors in data.service.ts

--- app/webFrontend/src/app/shared/data.service.ts
+++ app/webFrontend/src/app/shared/data.service.ts
@@ -1,110 +1,116 @@
-import { Injectable } from '@angular/core';
-import { BackendService } from './backend.service';
+import {Injectable} from '@angular/core';
+import {BackendService} from './backend.service';
 
 
 @Injectable()
 export class DataService {
 
-    items: any[] = [];
+  items: any[] = [];
 
-    constructor(public apiPath: string, public backendService: BackendService, public changeProps2Date?: string[], public dependentID?: string) {}
-
-    createItem(createItem: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.backendService.post(this.apiPath, JSON.stringify(createItem))
-                .subscribe(
-                    (responseItem: any) => {
-                        this.items.push(responseItem);
-                        resolve(responseItem);
-                    },
-                    error => reject(error)
-                )
-        })
+  static changeStringProp2DateProp(item: any, prop: string) {
+    if (item[prop] !== null) {
+      item[prop] = (typeof item[prop] === 'string' ? new Date(item[prop]) : null);
     }
+  }
 
-    readItems(): Promise<any[]> {
-        return new Promise((resolve, reject) => {
-            this.backendService.get(this.apiPath)
-                .subscribe(
-                    (responseItems: any) => {
-                        if (this.changeProps2Date) {
-                            responseItems.forEach(item => {
-                                this.changeProps2Date.forEach(prop => {
-                                    DataService.changeStringProp2DateProp(item, prop);
-                                });
-                            });
-                        }
+  constructor(public apiPath: string, public backendService: BackendService,
+              public changeProps2Date?: string[],
+              public dependentID?: string) {
+  }
 
-                        this.items = responseItems;
-                        resolve(responseItems)
-                    },
-                    error => reject(error)
-                )
-        })
-    }
+  createItem(createItem: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.backendService.post(this.apiPath, JSON.stringify(createItem))
+        .subscribe(
+          (responseItem: any) => {
+            this.items.push(responseItem);
+            resolve(responseItem);
+          },
+          error => reject(error)
+        );
+    });
+  }
 
-    updateItem(updateItem: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.backendService.put(this.apiPath + updateItem._id, JSON.stringify(updateItem))
-                .subscribe(
-                    (res) => {
-                          resolve(res)
-                    },
-                    error => reject(error)
-                )
-        })
-    }
+  readItems(): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this.backendService.get(this.apiPath)
+        .subscribe(
+          (responseItems: any) => {
+            if (this.changeProps2Date) {
+              responseItems.forEach(item => {
+                this.changeProps2Date.forEach(prop => {
+                  DataService.changeStringProp2DateProp(item, prop);
+                });
+              });
+            }
 
-    deleteItem(deleteItem: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.backendService.delete(this.apiPath + deleteItem._id)
-                .subscribe(
-                    () => {
-                        this.items = this.items.filter(item => {return item._id !== deleteItem._id});
-                        resolve()
-                    },
-                    error => reject(error)
-                )
-        })
-    }
+            this.items = responseItems;
+            resolve(responseItems);
+          },
+          error => reject(error)
+        );
+    });
+  }
 
-    readSingleItem(id: string): Promise<any[]> {
-        return new Promise((resolve, reject) => {
-            this.backendService.get(this.apiPath + id)
-                .subscribe(
-                    (responseItems: any) => {
-                        if (this.changeProps2Date) {
-                            responseItems.forEach(item => {
-                                this.changeProps2Date.forEach(prop => {
-                                    DataService.changeStringProp2DateProp(item, prop);
-                                });
-                            });
-                        }
+  updateItem(updateItem: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.backendService.put(this.apiPath + updateItem._id, JSON.stringify(updateItem))
+        .subscribe(
+          (res) => {
+            resolve(res);
+          },
+          error => reject(error)
+        );
+    });
+  }
 
-                        this.items = responseItems;
-                        resolve(responseItems)
-                    },
-                    error => reject(error)
-                )
-        })
-    }
+  deleteItem(deleteItem: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.backendService.delete(this.apiPath + deleteItem._id)
+        .subscribe(
+          () => {
+            this.items = this.items.filter(item => {
+              return item._id !== deleteItem._id;
+            });
+            resolve();
+          },
+          error => reject(error)
+        );
+    });
+  }
 
+  readSingleItem(id: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this.backendService.get(this.apiPath + id)
+        .subscribe(
+          (responseItems: any) => {
+            if (this.changeProps2Date) {
+              responseItems.forEach(item => {
+                this.changeProps2Date.forEach(prop => {
+                  DataService.changeStringProp2DateProp(item, prop);
+                });
+              });
+            }
 
-
-    static changeStringProp2DateProp(item: any, prop: string){
-        if (item[prop] !== null){
-            item[prop] = (typeof item[prop] === "string" ? new Date(item[prop]) : null)
-        }
-    }
-
-
+            this.items = responseItems;
+            resolve(responseItems);
+          },
+          error => reject(error)
+        );
+    });
+  }
 }
-
 
 @Injectable()
 export class CourseService extends DataService {
-    constructor(public backendService: BackendService) {
-        super('courses/', backendService);
-    }
+  constructor(public backendService: BackendService) {
+    super('courses/', backendService);
+  }
 }
 
+@Injectable()
+export class UserDataService extends DataService {
+  constructor(public backendService: BackendService) {
+    super('users/', backendService);
+  }
+}
