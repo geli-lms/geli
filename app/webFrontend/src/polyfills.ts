c0c21aed718a71105c21b21696a2fb7e1cd2e796
Alexander Eimer <alexander.eimer@gmail.com>
2017-04-18 12:02:15 +0200

    changed reuqired zone js

--- app/webFrontend/src/polyfills.ts
+++ app/webFrontend/src/polyfills.ts
@@ -16,4 +16,5 @@ import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
+//import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone.js';
