3284f3a2919cade12ac5ab7a75a5989b585227ee
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-04-20 15:26:25 +0200

    Fixing code formatting of backend user controller, #6

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -11,43 +11,43 @@ import {IUserModel, User} from "../models/User";
 @JsonController("/user")
 export class UserController {
 
-    static generateToken(user: IUser) {
-        return sign(
-            {_id: user._id},
-            config.secret,
-            {
-                expiresIn: 10080 // in seconds
-            }
-        );
-    }
+  static generateToken(user: IUser) {
+    return sign(
+      {_id: user._id},
+      config.secret,
+      {
+        expiresIn: 10080 // in seconds
+      }
+    );
+  }
 
-    @Post("/login")
-    @UseBefore(bodyParserJson(), passportLoginMiddleware) // We need body-parser for passport to find the credentials
-    postLogin(@Req() request: Request) {
-        const user = <IUserModel>(<any>request).user;
+  @Post("/login")
+  @UseBefore(bodyParserJson(), passportLoginMiddleware) // We need body-parser for passport to find the credentials
+  postLogin(@Req() request: Request) {
+    const user = <IUserModel>(<any>request).user;
 
+    return {
+      token: "JWT " + UserController.generateToken(user),
+      user: user.toObject()
+    };
+  }
+
+  @Post("/register")
+  postRegister(@Body() user: IUser, @Res() res: Response) {
+    return User.findOne({email: user.email})
+      .then((existingUser) => {
+        // If user is not unique, return error
+        if (existingUser) {
+          throw new HttpError(422, "That email address is already in use.");
+        }
+
+        return new User(user).save();
+      })
+      .then((user) => {
         return {
-            token: 'JWT ' + UserController.generateToken(user),
-            user: user.toObject()
+          token: "JWT " + UserController.generateToken(user),
+          user: user.toObject()
         };
-    }
-
-    @Post("/register")
-    postRegister(@Body() user: IUser, @Res() res: Response) {
-        return User.findOne({email: user.email})
-            .then((existingUser) => {
-                // If user is not unique, return error
-                if (existingUser) {
-                    throw new HttpError(422, "That email address is already in use.");
-                }
-
-                return new User(user).save();
-            })
-            .then((user) => {
-                return {
-                    token: 'JWT ' + UserController.generateToken(user),
-                    user: user.toObject()
-                };
-            });
-    }
+      });
+  }
 }
