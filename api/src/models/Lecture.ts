9a5f687b53c74bc52c1d5073fcc6e339f7cd7af5
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-04-20 19:36:06 +0200

    Transform IDs of models in backend to get fix some frontend problems, #26

--- api/src/models/Lecture.ts
+++ api/src/models/Lecture.ts
@@ -6,23 +6,29 @@ interface ILectureModel extends ILecture, mongoose.Document {
 }
 
 const lectureSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String
-        },
-        units: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: Unit
-            }
-        ]
+    name: {
+      type: String,
+      required: true
     },
-    {
-        timestamps: true
+    description: {
+      type: String
+    },
+    units: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Unit
+      }
+    ]
+  },
+  {
+    timestamps: true,
+    toObject: {
+      transform: function(doc: any, ret: any) {
+        ret._id = ret.id;
+        delete ret.id;
+      }
     }
+  }
 );
 
 
