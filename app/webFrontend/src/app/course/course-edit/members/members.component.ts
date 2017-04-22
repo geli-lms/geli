7af699d17b6b0fd748b468f9228c0ba625159d9a
Oliver Neff <oliver.neff@stud.h-da.de>
2017-04-22 12:56:51 +0200

    Implement example datas for (#7) and add basic html implementation.

--- app/webFrontend/src/app/course/course-edit/members/members.component.ts
+++ app/webFrontend/src/app/course/course-edit/members/members.component.ts
@@ -9,7 +9,13 @@ import {Component, Input, OnInit} from '@angular/core';
 export class MembersComponent implements OnInit {
   @Input() course;
 
+  members: { email: string, role: string, profile: { firstname: string, lastname: string }}[] = [];
+
   constructor() {
+    this.members[0] = {email: 'MaxMüller@gmx.de', role: 'student', profile: {firstname: 'Max', lastname: 'Müller'}};
+    this.members[1] = {email: 'FrankWalther@aol.com', role: 'student', profile: {firstname: 'Frank', lastname: 'Walther'}};
+    this.members[2] = {email: 'PaulRiebmann@test.com', role: 'teacher', profile: {firstname: 'Paul', lastname: 'Riebmann'}};
+    this.members[3] = {email: 'SvenjaMüller@google.de', role: 'admin', profile: {firstname: 'Svenja', lastname: 'Müller'}};
   }
 
   ngOnInit() {
