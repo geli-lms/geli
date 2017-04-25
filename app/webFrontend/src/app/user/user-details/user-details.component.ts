7f85ad2f0a8ee5827ebc1efc55719a71f8c9a58b
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-04-25 22:53:28 +0200

    Starting implementation of user-details.component, #5

--- app/webFrontend/src/app/user/user-details/user-details.component.ts
+++ app/webFrontend/src/app/user/user-details/user-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserDataService} from '../../shared/data.service';
+import {Router} from '@angular/router';
+import {User} from '../../models/user';
+import {UserService} from '../../shared/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(private userDataService: UserDataService,
+              private router: Router) {
+    if(this.router.url === 'profile') {
+      let token = localStorage.getItem('currentUserToken');
+    }
+  }
 
   ngOnInit() {
+  }
+
+  getUser(userId: string) {
+    this.userDataService.readSingleItem(userId).then(user => {
+      this.user = user[0];
+    });
   }
 
 }
