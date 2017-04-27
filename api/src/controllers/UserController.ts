62634832b2daf389121b58947c6261fc7c01ef54
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-04-27 17:00:53 +0200

    Moving login and register paths to AuthController

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,53 +1,60 @@
 import {sign} from 'jsonwebtoken';
 import {Request, Response} from 'express';
-import {Body, Post, JsonController, Req, Res, HttpError, UseBefore} from 'routing-controllers';
+import {Body, Post, JsonController, Req, Res, HttpError, UseBefore, Get, Param, Put} from 'routing-controllers';
 import {json as bodyParserJson} from 'body-parser';
-import passportLoginMiddleware from '../security/passportLoginMiddleware';
+import passportJwtMiddleware from '../security/passportJwtMiddleware';
 
 import config from '../config/main';
 import {IUser} from '../models/IUser';
 import {IUserModel, User} from '../models/User';
 
 @JsonController('/user')
+@UseBefore(passportJwtMiddleware)
 export class UserController {
 
-    static generateToken(user: IUser) {
-        return sign(
-            {_id: user._id},
-            config.secret,
-            {
-                expiresIn: 10080 // in seconds
-            }
-        );
-    }
+  static generateToken(user: IUser) {
+    return sign(
+      {_id: user._id},
+      config.secret,
+      {
+        expiresIn: 10080 // in seconds
+      }
+    );
+  }
 
-    @Post('/login')
-    @UseBefore(bodyParserJson(), passportLoginMiddleware) // We need body-parser for passport to find the credentials
-    postLogin(@Req() request: Request) {
-        const user = <IUserModel>(<any>request).user;
+  @Get('/')
+  getUsers() {
+    return User.find({})
+      .then((users) => users.map((user) => user.toObject({ virtuals: true})));
+  }
 
-        return {
-            token: 'JWT ' + UserController.generateToken(user),
-            user: user.toObject()
-        };
-    }
+  @Get('/roles')
+  getRoles() {
+    return User.schema.path('role').enumValues;
+  }
 
-    @Post('/register')
-    postRegister(@Body() user: IUser, @Res() res: Response) {
-        return User.findOne({email: user.email})
-            .then((existingUser) => {
-                // If user is not unique, return error
-                if (existingUser) {
-                    throw new HttpError(422, 'That email address is already in use.');
-                }
+  @Get('/:id')
+  getUser(@Param('id') id: string) {
+    return User.findById(id)
+      .then((user) => user.toObject());
+  }
 
-                return new User(user).save();
-            })
-            .then((savedUser) => {
-                return {
-                    token: 'JWT ' + UserController.generateToken(savedUser),
-                    user: savedUser.toObject()
-                };
-            });
-    }
+  @Put('/:id')
+  updateUser(@Param('id') id: string, @Body() user: IUser) {
+    return User.find({'role': 'admin'})
+      .then((adminUsers) => {
+        console.log('AdminUsers: ' + adminUsers.length);
+        const _id = adminUsers[0]._id;
+        const idTest = adminUsers[0].get('id');
+        if (adminUsers.length === 1 &&
+            adminUsers[0].get('id') === id &&
+            adminUsers[0].role === 'admin' &&
+            user.role !== 'admin') {
+          throw new HttpError(400, 'There are no other users with admin privileges.');
+        } else {
+          return User.findByIdAndUpdate(id, user, {'new': true});
+        }
+      })
+      .then((savedUser) => savedUser.toObject());
+  }
 }
