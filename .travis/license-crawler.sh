9ae18263d63681cdc37fe475e799da3a68131e29
Alexander Eimer <alexander.eimer@gmail.com>
2017-05-03 15:59:58 +0200

    now using .travis/node_modules with nlf

--- .travis/license-crawler.sh
+++ .travis/license-crawler.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
+# Variables
 CSV_FILE="nlf-licenses.csv"
 CSV_FILE_APACHE="nlf-licenses.apache.csv"
+MODULE_PATH=".travis/node_modules"
+NLF_PATH="nlf/bin"
 
+# Functions
+function npm_package_is_installed {
+  local return_=1
+  ls node_modules | grep $1 > /dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
+
+# Begin of code
 echo
 echo +++ Run NLF to search for Apache-Licenses +++
 echo
 
+echo + checking if nlf is installed
+cd .travis
+if [ $(npm_package_is_installed nlf) == 0 ]; then
+  echo "+ ERROR: nlf is not installed, please add nlf to the .travis/package.json"
+  exit 1
+fi
+cd ..
+
 echo + going to folder api
 cd api
 
-echo + installing nlf
-npm install nlf
-
 echo + crawling licenses from api
-./node_modules/nlf/bin/nlf-cli.js --csv > ../$CSV_FILE
+../$MODULE_PATH/$NLF_PATH/nlf-cli.js --csv > ../$CSV_FILE
 
 echo + going to folder app/webFrontend
 cd ../app/webFrontend
 
 echo + crawling licenses from app/webFrontend
 # append frontend licenses to existing csv; we have to skip the first line (header)
-../../api/node_modules/nlf/bin/nlf-cli.js --csv | awk '{if(NR>1)print}'  >> ../../$CSV_FILE
+../../$MODULE_PATH/$NLF_PATH/nlf-cli.js --csv | awk '{if(NR>1)print}'  >> ../../$CSV_FILE
 
 echo + going back to root folder
 cd ../..
