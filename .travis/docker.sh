45f9d77a28f281966f8edc60de0ba1c9c8d3010b
Lukas Korte <lukas@korte.me>
2017-05-07 10:59:23 +0200

    Tag docker images with "latest"

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -2,6 +2,6 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker push hdafbi/geli-api:$TRAVIS_BRANCH;
-    docker push hdafbi/geli-web-frontend:$TRAVIS_BRANCH;
+    docker push hdafbi/geli-api;
+    docker push hdafbi/geli-web-frontend;
 fi
