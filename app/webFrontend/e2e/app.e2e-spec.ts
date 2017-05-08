e171b06fc9d40a034f41e4333c36eabf4a12f33b
Lukas Korte <lukas@korte.me>
2017-05-08 13:45:11 +0200

    Fix e2e test

--- app/webFrontend/e2e/app.e2e-spec.ts
+++ app/webFrontend/e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('web-frontend App', () => {
     page = new WebFrontendPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to LearnIT');
+    expect(page.getParagraphText()).toEqual('Welcome to geli!');
   });
 });
