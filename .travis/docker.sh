0e7ba613dcadc43333921a32612b5d0d872f4ac2
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-05-08 18:16:25 +0200

    move docker build to docker.sh and only do a build if its not a pullrequest

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    docker build -t hdafbi/geli-api:latest -f .docker/api/Dockerfile .
+    docker tag hdafbi/geli-api hdafbi/geli-api:$TRAVIS_BRANCH
+    docker build -t hdafbi/geli-web-frontend:latest -f .docker/web-frontend/Dockerfile .
+    docker tag hdafbi/geli-web-frontend hdafbi/geli-web-frontend:$TRAVIS_BRANCH
+
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
     docker push hdafbi/geli-api;
+    docker push hdafbi/geli-api:$TRAVIS_BRANCH;
     docker push hdafbi/geli-web-frontend;
+    docker push hdafbi/geli-web-frontend:$TRAVIS_BRANCH;
+  fi
 fi
