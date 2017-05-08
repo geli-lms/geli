6d5d9838fe420866ada269298d99084738dfa13d
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-05-08 19:58:12 +0200

    add some echo to docker.sh

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -2,15 +2,21 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
   if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "build docker images";
     docker build -t hdafbi/geli-api:latest -f .docker/api/Dockerfile .
     docker tag hdafbi/geli-api hdafbi/geli-api:$TRAVIS_BRANCH
     docker build -t hdafbi/geli-web-frontend:latest -f .docker/web-frontend/Dockerfile .
     docker tag hdafbi/geli-web-frontend hdafbi/geli-web-frontend:$TRAVIS_BRANCH
 
+    echo "publish docker images";
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
     docker push hdafbi/geli-api;
     docker push hdafbi/geli-api:$TRAVIS_BRANCH;
     docker push hdafbi/geli-web-frontend;
     docker push hdafbi/geli-web-frontend:$TRAVIS_BRANCH;
+  else
+    echo "omit docker build and publish. (pull request: #$TRAVIS_PULL_REQUEST)";
   fi
+else
+  echo "omit docker build and publish. (wrong branch: $TRAVIS_BRANCH)";
 fi
