b83fe6fbee6561f89cda497617b1fe6f3f0237f5
Alexander Eimer <alexander.eimer@gmail.com>
2017-05-30 21:01:14 +0200

    Adds utf8 encoding option

--- .travis/license.js
+++ .travis/license.js
@@ -67,7 +67,7 @@ nlf.find({
     data = data.replace(SEARCH_FOR, out);
 
     if (process.env.TRAVIS) {
-      fs.writeFile(PATH_APP_WEB + PATH_APP_WEB_FILE_NAME, data,
+      fs.writeFile(PATH_APP_WEB + PATH_APP_WEB_FILE_NAME, data, 'utf8',
         () => console.log(('+ appWeb: wrote file'))
       );
     } else {
