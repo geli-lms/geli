20aec8f3160d1957cca41f848d945d36a28533f3
Alexander Eimer <alexander.eimer@gmail.com>
2017-05-30 22:12:45 +0200

    Adds all contributor github-accounts

--- app/webFrontend/src/app/about/general-info/contributors.ts
+++ app/webFrontend/src/app/about/general-info/contributors.ts
@@ -9,14 +9,14 @@ export class ContributorsList {
       , new Contributor('Ute', 'Trapp', '17SuSe', 'Lecturer', 'utetrapp')
       , new Contributor('David', 'Müller', '17SuSe', 'Lecturer', 'd89')
       // Students
-      , new Contributor('Bernd', 'Noetscher', '17SuSe', 'Student')
-      , new Contributor('Felix', 'Brucker', '17SuSe', 'Student')
+      , new Contributor('Bernd', 'Noetscher', '17SuSe', 'Student', 'bernd-hda')
+      , new Contributor('Felix', 'Brucker', '17SuSe', 'Student', 'felixbrucker')
       , new Contributor('Alexander', 'Eimer', '17SuSe', 'Student', 'aeimer')
-      , new Contributor('Steffen', 'Großpersky', '17SuSe', 'Student')
+      , new Contributor('Steffen', 'Großpersky', '17SuSe', 'Student', 'grosspersky')
       , new Contributor('Ken', 'Hasenbank', '17SuSe', 'Student', 'khase')
-      , new Contributor('Lukas', 'Korte', '17SuSe', 'Student')
-      , new Contributor('Oliver', 'Neff', '17SuSe', 'Student')
-      , new Contributor('Alexaner', 'Weinfurter', '17SuSe', 'Student')
+      , new Contributor('Lukas', 'Korte', '17SuSe', 'Student', 'lukaskorte')
+      , new Contributor('Oliver', 'Neff', '17SuSe', 'Student', 'OliverNeff')
+      , new Contributor('Alexaner', 'Weinfurter', '17SuSe', 'Student', 'alexweinfurter')
     ];
   }
 }
