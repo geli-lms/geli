09bbce174acc3d372426ec04816a11358b0ba7d5
Felix Brucker <felix.brucker@live.de>
2017-06-06 14:08:33 +0200

    Add accessKey to frontend

---
+++ app/webFrontend/src/app/shared/components/access-key-dialog/access-key-dialog.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {MdDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'access-key-dialog',
+  templateUrl: './access-key-dialog.component.html',
+  styleUrls: ['./access-key-dialog.component.css']
+})
+export class AccessKeyDialog {
+  constructor(public dialogRef: MdDialogRef<AccessKeyDialog>) {}
+}
