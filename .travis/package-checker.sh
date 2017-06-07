32c9cdc0bc2fd79f33fcaffe91bf8dcfc283dc66
Alexander Eimer <alexander.eimer@gmail.com>
2017-06-07 14:12:16 +0200

    Installs david local

--- .travis/package-checker.sh
+++ .travis/package-checker.sh
@@ -4,25 +4,28 @@ echo
 echo "+++ David dependency checker +++"
 echo
 
-npm i --global david
+npm i david
 
 echo "+ checking .travis"
 cd .travis
-# node_modules/.bin/david
-david
+pwd
+node_modules/.bin/david
+# david
 echo
 cd ..
 
 echo "+ checking api"
 cd api
-# ../.travis/node_modules/.bin/david
-david
+pwd
+../.travis/node_modules/.bin/david
+# david
 echo
 cd ..
 
 echo "+ checking app/webFrontend"
 cd app/webFrontend
-# ../../.travis/node_modules/.bin/david
-david
+pwd
+../../.travis/node_modules/.bin/david
+# david
 echo
 cd ../..
