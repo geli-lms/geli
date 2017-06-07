f1a04cb809513805501dc067bbfc12975cd98fc4
Oliver Neff <oliver.neff@stud.h-da.de>
2017-06-08 00:32:53 +0200

    Implement parser

--- api/src/common/obsCsvParser.ts
+++ api/src/common/obsCsvParser.ts
@@ -2,24 +2,28 @@
  * Created by olineff on 05.06.2017.
  */
 import {Course} from '../models/Course';
+import {IUser} from '../../../shared/models/IUser';
 
 export class ObsCsvParser {
   course: any;
   lines: string[] = [];
-  allUsers: any[] = [];
   user: {firstName: string, lastName: string, uid: string}[] = [];
 
-
   public work(file: any, course: any, user: any[]): any {
+    // TODO Validierung
     this.splitByLineBreaks(file.buffer.toString());
-    return null;
+    console.log('File was parsed successfully. There where' + this.user.length +  'user parsed.');
+    return course;
   }
 
   private splitByLineBreaks(buffer: string) {
     this.lines = buffer.split(/\r?\n|\r/);
     const userLines = this.lines.filter(e => e.split(';').length >= 3);
     userLines.forEach(e => this.user.push({firstName: e.split(';')[0], lastName: e.split(';')[1], uid: e.split(';')[2]}));
-    console.log(this.allUsers);
+  }
+
+  private addParsedUsersToCourse() {
+
   }
 
 }
