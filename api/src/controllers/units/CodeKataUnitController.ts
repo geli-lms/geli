9a5806e86edf10118e8e65fa9e889c3db77b1ea7
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-06-13 19:11:02 +0200

    create backend CodeKata controller

---
+++ api/src/controllers/units/CodeKataUnitController.ts
@@ -0,0 +1,25 @@
+import {Body, Post, JsonController, UseBefore, BadRequestError} from 'routing-controllers';
+import passportJwtMiddleware from '../../security/passportJwtMiddleware';
+import {UnitController} from './UnitController';
+import {CodeKataUnit} from '../../models/units/CodeKataUnit';
+
+@JsonController('/units/codekata')
+@UseBefore(passportJwtMiddleware)
+export class CodeKataUnitController extends UnitController {
+
+  @Post('/')
+  addCodeKataUnit(@Body() data: any) {
+    // discard invalid requests
+    if (!data.lectureId) {
+      return new BadRequestError('No lecture ID was submitted.');
+    }
+
+    if (!data.codeKataUnit) {
+      return new BadRequestError('No codekata unit was submitted.');
+    }
+
+    return new CodeKataUnit(data.codeKataUnit).save().then((savedCodeKataUnit) => {
+      this.pushToLecture(data.lectureId, savedCodeKataUnit);
+    });
+  }
+}
