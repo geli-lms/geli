279071e18ccf63b90d9b5abba0887e3d2501418d
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-06-13 18:51:04 +0200

    create models and mongoose shema for code katas

---
+++ api/src/models/units/CodeKataUnit.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose';
+import {Unit} from './Unit';
+import {ICodeKataUnit} from '../../../../shared/models/units/ICodeKataUnit';
+
+interface ICodeKataModel extends ICodeKataUnit, mongoose.Document {
+}
+
+const codeKataSchema = new mongoose.Schema({
+  definition: {
+    type: String
+  },
+  code: {
+    type: String
+  },
+  test: {
+    type: String
+  }
+});
+
+const CodeKataUnit = Unit.discriminator('codeKata', codeKataSchema);
+
+export {CodeKataUnit, ICodeKataModel}
