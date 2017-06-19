99713ee328451efbb7cfa5707633e4a56d6ce8d7
AlexWeinfurter <alexander_weinfurter@hotmail.de>
2017-06-19 20:58:35 +0200

    Merge branch 'feature/84-multiple-teachers' of https://github.com/h-da/geli into feature/84-multiple-teachers
    # Conflicts:
    #	api/fixtures/userFixtures.ts
    #	app/webFrontend/src/app/app.module.ts
    #	app/webFrontend/src/app/course/course-edit/teachers/teachers.component.html
    #	app/webFrontend/src/app/course/course-edit/teachers/teachers.component.scss
    #	app/webFrontend/src/app/course/course-edit/teachers/teachers.component.ts
    #	app/webFrontend/src/app/models/Course.ts
    #	app/webFrontend/src/app/shared/directives/gravatar.directive.ts
    #	app/webFrontend/src/app/shared/services/data.service.ts

---
+++ app/webFrontend/src/app/course/course-edit/course-user-list/course-user-list.component.ts
@@ -0,0 +1,80 @@
+import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
+import {DragulaService} from 'ng2-dragula';
+import {IUser} from '../../../../../../../shared/models/IUser';
+import {FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-course-user-list',
+  templateUrl: './course-user-list.component.html',
+  styleUrls: ['./course-user-list.component.scss', '../../../../../node_modules/dragula/dist/dragula.css']
+})
+export class CourseUserListComponent implements OnInit {
+
+  @Input() courseId;
+  @Input() usersInCourse;
+  @Input() users;
+  @Input() dragulaBagId;
+
+  currentMember: IUser = null;
+  fuzzySearch: String = '';
+  userCtrl: FormControl;
+  filteredStates: any;
+
+  @Output() onDragendUpdate = new EventEmitter<IUser[]>();
+  @Output() onRemove = new EventEmitter<String>();
+
+  setMember(member: IUser) {
+    this.currentMember = member;
+  }
+
+  constructor(private dragula: DragulaService) {
+    this.userCtrl = new FormControl();
+    this.filteredStates = this.userCtrl.valueChanges
+      .startWith(null)
+      .map(name => this.filterStates(name));
+  }
+
+  ngOnInit() {
+    this.dragula.setOptions(this.dragulaBagId, {
+      revertOnSpill: true
+    });
+    this.dragula.dragend.subscribe(value => {
+      this.onDragendUpdate.emit(this.usersInCourse);
+    });
+  }
+
+  ngOnDestroy() {
+    this.dragula.destroy(this.dragulaBagId);
+  }
+
+  filterStates(val: string) {
+    return val ? this.users.filter(s => this.fuzzysearch(val, s))
+      .map(e => e.profile.firstName + ' ' + e.profile.lastName + ' ' + e.email)
+      : [];
+  }
+
+  // TODO: bessere Suche (levenshtein)
+  fuzzysearch(toSearch: string, user: IUser): boolean {
+    const lowerToSearch: string = toSearch.toLowerCase();
+    const elementsToFind = lowerToSearch.split(' ');
+    if (user.uid === undefined) {
+      user.uid = '';
+    }
+    const resArray = elementsToFind.filter(e => user.profile.firstName.toLowerCase().includes(e) ||
+      user.profile.lastName.toLowerCase().includes(e) ||
+      user.email.toLowerCase().includes(e)
+    );
+    return resArray.length > 0;
+  }
+
+  /**
+   * @param _id Id of an user.
+   * @param direction direction where user to switch.
+   */
+  removeUser(id: string) {
+    this.onRemove.emit(id);
+  }
+
+
+
+}
