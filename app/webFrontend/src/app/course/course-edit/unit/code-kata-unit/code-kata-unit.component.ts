0615ed095f1ddc6d657faaa8db3024213879c957
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-06-22 15:33:55 +0200

    implement code-kata form for students

--- app/webFrontend/src/app/course/course-edit/unit/code-kata-unit/code-kata-unit.component.ts
+++ app/webFrontend/src/app/course/course-edit/unit/code-kata-unit/code-kata-unit.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {CodeKataUnit} from "../../../../models/CodeKataUnit";
 
 @Component({
   selector: 'app-code-kata',
@@ -6,30 +7,19 @@ import {Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./code-kata-unit.component.scss']
 })
 export class CodeKataComponent implements OnInit {
+  @Input() codeKata: CodeKataUnit;
+
   @ViewChild('definitionEditor') definitionEditor;
   @ViewChild('codeEditor') codeEditor;
   @ViewChild('testEditor') testEditor;
 
-  complete: string = '// Task: Manipulate the targetSet, so it only contains the values \"Hello\" and \"h_da\"\n\nlet targetSet = new Set([\"Hello\", \"there\"]);\n//####################\n\ntargetSet.add(\"h_da\");\ntargetSet.delete(\"there\");\n\n//####################\n\nvalidate();\n\nfunction validate() {\n  return targetSet.has(\"Hello\") && targetSet.has(\"h_da\") && targetSet.size === 2;\n}';
-
-  definition: string = 'definition';
-  code: string = '';
-  test: string = 'test';
-
-  logs: string = 'none';
+  logs: string;
 
   constructor() {
+    this.logs = null;
   }
 
   ngOnInit() {
-    const seperator: string = "\/\/#+";
-    let firstSeperator: number = this.findFirstIndexOf(this.complete, seperator);
-    let lastSeperator: number = this.findLastIndexOf(this.complete, seperator);
-
-    this.definition = this.complete.substring(0, firstSeperator).trim();
-
-    let tmp = this.complete.substring(lastSeperator, this.complete.length);
-    this.test = tmp.slice(tmp.search("\n")).trim();
   }
 
   ngAfterViewInit() {
@@ -44,23 +34,24 @@ export class CodeKataComponent implements OnInit {
     });
   }
 
+  // refactor this to use the same as in code-kata-unit-form
   private validate() {
-    let codeToTest: string = this.definition + '\n' + this.code + '\n' + this.test;
+    const codeToTest: string = this.codeKata.definition + "\n" + this.codeKata.code + "\n" + this.codeKata.test;
 
     this.logs = '';
     (<any>window).geli = {logs: ''};
-    let origLogger = window.console.log;
+    const origLogger = window.console.log;
     window.console.log = function (msg) {
       (<any>window).geli.logs += msg + '\n';
       origLogger(msg);
     };
-    let origErrorLogger = window.console.error;
+    const origErrorLogger = window.console.error;
     window.console.error = function (msg) {
       (<any>window).geli.logs += msg + '\n';
       origErrorLogger(msg);
     };
 
-    window.onerror = function(message, url, linenumber) {
+    window.onerror = function (message, url, linenumber) {
       console.log(message);
       console.log(linenumber);
     };
@@ -68,33 +59,17 @@ export class CodeKataComponent implements OnInit {
     console.log((<any>window).geli.logs);
     this.logs = (<any>window).geli.logs;
 
-    let result = eval(codeToTest);
+    const result = eval(codeToTest);
 
     window.console.log = origLogger;
     window.console.error = origErrorLogger;
-  }
 
-  private findFirstIndexOf(source: string, value: string): number {
-    return source.search(value);
-  }
-
-  private findLastIndexOf(source: string, value: string): number {
-    let regex = new RegExp(value, '');
-    let i: number = -1;
-
-    // limit execution time (prevent deadlocks)
-    let j = 10;
-    while (j > 0) {
-      j--;
-      let result = regex.exec(source.slice(++i));
-      if (result != null) {
-        i += result.index;
-      }
-      else {
-        i--;
-        break;
-      }
+    if (result === true || result === undefined) {
+      return true;
     }
-    return i;
+    else {
+      console.log(result);
+      return false;
+    }
   }
 }
