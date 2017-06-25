b88cab873d746740448dde9b9bd9e0736a110ac6
Felix Brucker <felix.brucker@live.de>
2017-06-25 22:04:42 +0200

    Allow multiple videos to be added to a unit, add file unit upload

--- api/src/controllers/UploadUnitController.ts
+++ api/src/controllers/UploadUnitController.ts
@@ -4,7 +4,8 @@ import crypto = require('crypto');
 const multer = require('multer');
 import passportJwtMiddleware from '../security/passportJwtMiddleware';
 
-import {VideoUnit} from '../models/units/VideoUnit';
+import {VideoUnit, IVideoUnitModel} from '../models/units/VideoUnit';
+import {FileUnit, IFileUnitModel} from '../models/units/FileUnit';
 import {UnitController} from './UnitController';
 
 const uploadOptions = {
@@ -26,27 +27,91 @@ const uploadOptions = {
 @UseBefore(passportJwtMiddleware)
 export class UploadUnitController extends UnitController {
 
-  @Post('/')
+  @Post('/video')
   addVideoUnit(@UploadedFile('file', {uploadOptions}) file: any, @Body() data: any) {
     // discard invalid requests
     if (!data.lectureId) {
+      fs.unlinkSync(file.path);
       throw new BadRequestError('Misssing lectureId');
     }
+    if (!data.courseId) {
+      fs.unlinkSync(file.path);
+      throw new BadRequestError('Misssing courseId');
+    }
     if (!data.name) {
+      fs.unlinkSync(file.path);
       throw new BadRequestError('Misssing name');
     }
 
-    // path for file upload units (for now video only)
     if (file) {
-      return new VideoUnit({
-        name: data.name,
-        description: data.description,
-        _course: data.courseId,
-        filePath: file.path,
-        fileName: file.originalname
-      })
-        .save()
-        .then((unit) => this.pushToLecture(data.lectureId, unit));
+      if (data.unitId) {
+        // add to existing unit
+        return VideoUnit.findById(data.unitId)
+          .then((unit) => {
+            (<IVideoUnitModel>unit).files.push({
+              path: file.path,
+              name: file.filename,
+              alias: file.originalname,
+            });
+            return unit.save();
+          })
+          .then((unit) => unit.toObject());
+      } else {
+        // create new unit
+        return new VideoUnit({
+          name: data.name,
+          description: data.description,
+          _course: data.courseId,
+          files: [{path: file.path, name: file.filename, alias: file.originalname}],
+        })
+          .save()
+          .then((unit) => ({lecture: this.pushToLecture(data.lectureId, unit), unit}))
+          .then(({lecture, unit}) => unit.toObject());
+      }
+    }
+  }
+
+  @Post('/file')
+  addFileUnit(@UploadedFile('file', {uploadOptions}) file: any, @Body() data: any) {
+    // discard invalid requests
+    if (!data.lectureId) {
+      fs.unlinkSync(file.path);
+      throw new BadRequestError('Misssing lectureId');
+    }
+    if (!data.courseId) {
+      fs.unlinkSync(file.path);
+      throw new BadRequestError('Misssing courseId');
+    }
+    if (!data.name) {
+      fs.unlinkSync(file.path);
+      throw new BadRequestError('Misssing name');
+    }
+
+    if (file) {
+      if (data.unitId) {
+        // add to existing unit
+        return FileUnit.findById(data.unitId)
+          .then((unit) => {
+            (<IFileUnitModel>unit).files.push({
+              path: file.path,
+              name: file.filename,
+              alias: file.originalname,
+            });
+            return unit.save();
+          })
+          .then((unit) => unit.toObject());
+      } else {
+        // create new unit
+        return new FileUnit({
+          name: data.name,
+          description: data.description,
+          _course: data.courseId,
+          files: [{path: file.path, name: file.filename, alias: file.originalname}],
+        })
+          .save()
+          .then((unit) => ({lecture: this.pushToLecture(data.lectureId, unit), unit}))
+          .then(({lecture, unit}) => unit.toObject());
+      }
     }
   }
 }
--- api/src/models/units/VideoUnit.ts
+++ api/src/models/units/VideoUnit.ts
@@ -6,12 +6,30 @@ interface IVideoUnitModel extends IVideoUnit, mongoose.Document {
 }
 
 const videoUnitSchema = new mongoose.Schema({
-  filePath: {
-    type: String,
+  files: [
+    {
+      path: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      alias: {
+        type: String,
+      },
+    }
+  ],
+}, {
+  toObject: {
+    transform: function (doc: any, ret: any) {
+      ret._id = ret._id.toString();
+      ret.files = ret.files.map((file: any) => {
+        file._id = file._id.toString();
+        return file;
+      });
+      ret._course = ret._course.toString();
+    }
   },
-  fileName: {
-    type: String,
-  }
 });
 
 const VideoUnit = Unit.discriminator('video', videoUnitSchema);
