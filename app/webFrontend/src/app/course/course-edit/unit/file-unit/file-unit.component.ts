b5f7b1500285f95e0edda9ce137cf12415e0b825
Felix Brucker <felix.brucker@live.de>
2017-06-25 22:09:35 +0200

    Add file uploads to frontend and allow editing

---
+++ app/webFrontend/src/app/course/course-edit/unit/file-unit/file-unit.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {IFileUnit} from '../../../../../../../../shared/models/units/IFileUnit';
+
+@Component({
+  selector: 'app-file-unit',
+  templateUrl: './file-unit.component.html',
+  styleUrls: ['./file-unit.component.scss']
+})
+export class FileUnitComponent implements OnInit {
+
+  @Input() fileUnit: IFileUnit;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isPicture(fileName: string) {
+    const pictureExt: any = [
+      'jpg',
+      'jpeg',
+      'png',
+      'apng',
+      'svg',
+      'bmp',
+      'ico',
+    ];
+    const extPos = fileName.lastIndexOf('.');
+    if (extPos === -1) {
+      return false;
+    }
+    const ext = fileName.substr(extPos + 1);
+    return pictureExt.includes(ext);
+  }
+
+}
