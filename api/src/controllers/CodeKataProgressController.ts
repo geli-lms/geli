14e5e40899b8d6b0424c08521303f1e64c68903c
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-06-25 17:07:11 +0200

    add code-kata progress route

---
+++ api/src/controllers/CodeKataProgressController.ts
@@ -0,0 +1,26 @@
+import {BadRequestError, Body, Param, Post, Put, UseBefore} from 'routing-controllers';
+import {CodeKataProgress} from '../models/CodeKataProgress';
+import {JsonController} from 'routing-controllers';
+import {ProgressController} from './ProgressController';
+import {ICodeKataProgress} from '../../../shared/models/ICodeKataProgress';
+
+
+@JsonController('/progress/code-katas')
+// @UseBefore(this.passportJwtMiddleware)
+export class CodeKataProgressController extends ProgressController {
+  @Post('/')
+  createProgress(@Body() data: any) {
+    // discard invalid requests
+    if (!data.course || !data.user || !data.unit) {
+      return new BadRequestError('progress need fields course, user and unit');
+    }
+
+    return new CodeKataProgress(data).save();
+  }
+
+  @Put('/:id')
+  updateCodeKataUnit(@Param('id') id: string, @Body() unit: ICodeKataProgress) {
+    return CodeKataProgress.findByIdAndUpdate(id, unit)
+      .then(u => u.toObject());
+  }
+}
