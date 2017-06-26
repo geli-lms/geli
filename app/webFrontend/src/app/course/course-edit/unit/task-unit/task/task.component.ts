16c00f7128bdf61b8de6dec06404e85c38a41f15
bernd-hda <bernd.noetscher@yahoo.de>
2017-06-26 21:46:10 +0200

    Fix 2nd typo in broken class

--- app/webFrontend/src/app/course/course-edit/unit/task-unit/task/task.component.ts
+++ app/webFrontend/src/app/course/course-edit/unit/task-unit/task/task.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {TaskAttestationService, TaskService} from '../../../../../shared/services/data.service';
+import {TaskAttestationService} from '../../../../../shared/services/data.service';
 import {MdSnackBar} from '@angular/material';
 import {UserService} from '../../../../../shared/services/user.service';
 
