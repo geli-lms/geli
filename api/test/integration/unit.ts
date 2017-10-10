709d191f8362215cf0f713487bd1c72e348f4f47
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-10-10 21:39:40 +0200

    Fix errors after api dependency updates

--- api/test/integration/unit.ts
+++ api/test/integration/unit.ts
@@ -20,25 +20,31 @@ describe('Unit', () => {
   beforeEach(() => fixtureLoader.load());
 
   describe(`POST ${BASE_URL}`, () => {
-    it('should upload a video and return the created unit', () => {
+    it('should upload a video and return the created unit', (done) => {
+      this.timeout(10000);
       return User.findOne({email: 'teacher@test.local'})
-        .then((user) => {
-          return Course.findOne({name: 'Introduction to web development'})
-            .then((course) => ({user, course}));
-      }).then(({user, course}) =>
-          chai.request(app)
-            .post(`${BASE_URL}/video`)
-            .field('name', 'Test Upload')
-            .field('description', 'This is my test upload.')
-            .field('lectureId', course.lectures[0].toString())
-            .field('courseId', course._id.toString())
-            .attach('file', fs.readFileSync('fixtures/binaryData/testvideo.mp4'), 'testvideo.mp4')
-            .set('Authorization', `JWT ${JwtUtils.generateToken(user)}`))
-        .then((res) => {
+      .then((user) => {
+        return Course.findOne({name: 'Introduction to web development'})
+        .then((course) => ({user, course}));
+      })
+      .then(({user, course}) => {
+        chai.request(app)
+        .post(`${BASE_URL}/video`)
+        .field('name', 'Test Upload')
+        .field('description', 'This is my test upload.')
+        .field('lectureId', course.lectures[0].toString())
+        .field('courseId', course._id.toString())
+        .attach('file', fs.readFileSync('fixtures/binaryData/testvideo.mp4'), 'testvideo.mp4')
+        .set('Authorization', `JWT ${JwtUtils.generateToken(user)}`)
+        .end((err, res) => {
           res.status.should.be.equal(200);
           res.body.name.should.be.equal('Test Upload');
           res.body.description.should.be.equal('This is my test upload.');
+
+          done();
         });
-    }).timeout(10000); // use higher timeout for upload to complete
+      })
+      .catch(done);
+    });
   });
 });
