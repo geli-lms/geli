af31d259dbf9021e4f0f2904b28246850a7853dc
Alexander Eimer <alexander.eimer@gmail.com>
2017-10-17 13:25:26 +0200

    Updates Contributor

--- app/webFrontend/src/app/about/general-info/contributors.ts
+++ app/webFrontend/src/app/about/general-info/contributors.ts
@@ -9,14 +9,23 @@ export class ContributorsList {
       , new Contributor('Ute', 'Trapp', '17SuSe', 'Lecturer', 'utetrapp')
       , new Contributor('David', 'Müller', '17SuSe', 'Lecturer', 'd89')
       // Students
-      , new Contributor('Bernd', 'Noetscher', '17SuSe', 'Student', 'bernd-hda')
-      , new Contributor('Felix', 'Brucker', '17SuSe', 'Student', 'felixbrucker')
-      , new Contributor('Alexander', 'Eimer', '17SuSe', 'Student', 'aeimer')
-      , new Contributor('Steffen', 'Großpersky', '17SuSe', 'Student', 'grosspersky')
-      , new Contributor('Ken', 'Hasenbank', '17SuSe', 'Student', 'khase')
-      , new Contributor('Lukas', 'Korte', '17SuSe', 'Student', 'lukaskorte')
-      , new Contributor('Oliver', 'Neff', '17SuSe', 'Student', 'OliverNeff')
-      , new Contributor('Alexaner', 'Weinfurter', '17SuSe', 'Student', 'alexweinfurter')
+      , new Contributor('Andreas', 'Schroll', '17WiSe', 'Student', 'AndreasSchroll')
+      , new Contributor('Jonas', 'Hess', '17WiSe', 'Student', 'JonasPHess')
+      , new Contributor('Sinisa', 'Jovanovic', '17WiSe', 'Student', 'jsinisa')
+      , new Contributor('Patrick', 'Skowronek', '17WiSe', 'Student', 'PatrickSkowronek')
+      , new Contributor('Henrik', 'Ochs', '17WiSe', 'Student', 'HPunktOchs')
+      , new Contributor('Patrick', 'Schmelmer', '17WiSe', 'Student', 'shelmz')
+      , new Contributor('Michael', '???', '17WiSe', 'Student', 'micpah')
+
+      , new Contributor('Felix', 'Brucker', '17SuSe', 'Student', 'felixbrucker', '17WiSe')
+      , new Contributor('Alexander', 'Eimer', '17SuSe', 'Student', 'aeimer', '17WiSe')
+      , new Contributor('Steffen', 'Großpersky', '17SuSe', 'Student', 'grosspersky', '17WiSe')
+      , new Contributor('Ken', 'Hasenbank', '17SuSe', 'Student', 'khase', '17WiSe')
+      , new Contributor('Lukas', 'Korte', '17SuSe', 'Student', 'lukaskorte', '17WiSe')
+      , new Contributor('Oliver', 'Neff', '17SuSe', 'Student', 'OliverNeff', '17WiSe')
+      , new Contributor('Alexaner', 'Weinfurter', '17SuSe', 'Student', 'alexweinfurter', '17WiSe')
+      // Old Students
+      , new Contributor('Bernd', 'Noetscher', '17SuSe', 'Student', 'bernd-hda', '17SuSe')
     ];
   }
 }
