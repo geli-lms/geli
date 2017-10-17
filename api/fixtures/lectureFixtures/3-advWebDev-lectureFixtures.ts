b76b2c1cbb2b2e2a88ed319ed84a3bfb460227a9
Ken Hasenbank <Ken.Hasenbank@stud.h-da.de>
2017-10-17 21:58:55 +0200

    add some more Fixtures

---
+++ api/fixtures/lectureFixtures/3-advWebDev-lectureFixtures.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:max-line-length */
+import {IFixture} from '../IFixture';
+import {Lecture} from '../../src/models/Lecture';
+
+export const lectureFixtures2: IFixture = {
+  Model: Lecture,
+  data: [
+    {
+      name: 'Coding Train',
+      description: 'evaluate your coding skillz',
+    },
+  ]
+};
