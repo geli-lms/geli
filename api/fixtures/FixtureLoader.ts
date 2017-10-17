f6790bcac9a905bc00ab89863d921ef5ed289048
Ken Hasenbank <Ken.Hasenbank@stud.h-da.de>
2017-10-17 22:00:55 +0200

    assign random teacher and a subset of students to each course

--- api/fixtures/FixtureLoader.ts
+++ api/fixtures/FixtureLoader.ts
@@ -3,18 +3,30 @@ import config from '../src/config/main';
 
 import {studentFixtures} from './UserFixtures/studentFixtures';
 import {courseFixtures} from './courseFixtures';
-import {lectureFixtures1} from './lectureFixtures/1-lectureFixtures';
+import {dumpTestLectureFixtures} from './lectureFixtures/1-dumpTest-lectureFixtures';
 import {IFixture} from './IFixture';
-import {Course, ICourseModel} from '../src/models/Course';
+import {ICourseModel} from '../src/models/Course';
 import {ILectureModel} from '../src/models/Lecture';
 import {adminFixtures} from './UserFixtures/adminFixtures';
 import {teacherFixtures} from './UserFixtures/teacherFixtures';
 import {tutorFixtures} from './UserFixtures/tutorFixtures';
-import {lectureFixtures2} from './lectureFixtures/2-lectureFixtures';
-import {freeTextFixture1u1} from './unitFixtures/1-1-freeTextFixture';
-import {freeTextFixture1u2} from './unitFixtures/1-2-freeTextFixture';
-import {codeKataFixture2u1} from './unitFixtures/2-1-codeKataFixtures';
+import {webDevelopmentLectureFixtures} from './lectureFixtures/2-intWebDev-lectureFixtures';
+import {dummyFreeTextFixture} from './unitFixtures/dummyFreeTextFixture';
+import {randomFreeTextFixture} from './unitFixtures/randomFreeTextFixture';
+import {simpleCodeKataFixture} from './unitFixtures/simpleCodeKataFixtures';
 import {IUnitModel} from '../src/models/units/Unit';
+import {advancedWebDevelopmentLectureFixtures} from './lectureFixtures/3-advWebDev-lectureFixtures';
+import {computerGraphicsLectureFixtures} from './lectureFixtures/4-comGra-lectureFixtures';
+import {randomLectureFixtures} from './lectureFixtures/5-randomness-lectureFixtures';
+import {mpseLectureFixtures} from './lectureFixtures/6-MPSE-lectureFixtures';
+import {IUserModel, User} from '../src/models/User';
+import {isNullOrUndefined} from 'util';
+import {loremFreeTextFixture} from './unitFixtures/loremFreeTextFixture';
+import {readmeFreeTextFixture} from './unitFixtures/readmeFreeTextFixture';
+import {mpseIntFreeTextFixture} from './unitFixtures/mpseIntFreeTextFixture';
+import {studentFreeTextFixture} from './unitFixtures/studentFreeTextFixture';
+import {hardCodeKataFixture} from './unitFixtures/hardCodeKataFixtures';
+import {studentCodeKataFixture} from './unitFixtures/studentCodeKataFixtures';
 
 export class FixtureLoader {
 
@@ -30,18 +42,47 @@ export class FixtureLoader {
   ];
 
   private lectures: Array<IFixture> = [
-    lectureFixtures1,
-    lectureFixtures2,
+    dumpTestLectureFixtures,
+    webDevelopmentLectureFixtures,
+    advancedWebDevelopmentLectureFixtures,
+    computerGraphicsLectureFixtures,
+    randomLectureFixtures,
+    mpseLectureFixtures,
   ];
 
   private units = [
-    [
-      [freeTextFixture1u1],
-      [freeTextFixture1u2],
+    [ // dumpTestLectureFixtures
+      [loremFreeTextFixture],
+      [dummyFreeTextFixture, randomFreeTextFixture],
+      [dummyFreeTextFixture],
     ],
-    [
-      [codeKataFixture2u1],
-    ]
+    [ // webDevelopmentLectureFixtures
+      [randomFreeTextFixture],
+      [simpleCodeKataFixture],
+      [dummyFreeTextFixture],
+    ],
+    [ // advancedWebDevelopmentLectureFixtures
+      [loremFreeTextFixture],
+      [hardCodeKataFixture],
+      [randomFreeTextFixture],
+    ],
+    [ // computerGraphicsLectureFixtures
+      [loremFreeTextFixture],
+      [dummyFreeTextFixture],
+      [randomFreeTextFixture],
+      [dummyFreeTextFixture],
+    ],
+    [ // randomLectureFixtures
+      [randomFreeTextFixture],
+      [randomFreeTextFixture],
+      [loremFreeTextFixture],
+      [randomFreeTextFixture],
+    ],
+    [ // mpseLectureFixtures
+      [mpseIntFreeTextFixture],
+      [readmeFreeTextFixture],
+      [studentFreeTextFixture, studentCodeKataFixture],
+    ],
   ];
 
   constructor() {
@@ -70,14 +111,33 @@ export class FixtureLoader {
       Promise.all(
         this.courses.map((courseFixtures) =>
           Promise.all(
-            courseFixtures.data.map((course) =>
-              new courseFixtures.Model(course).save()
-            )
+            courseFixtures.data.map((course) => {
+              // add random teacher as course admin
+              // add 2-10 random students
+              const tmp = <ICourseModel>course;
+              return Promise.all([this.getRandomTeacher(), this.getRandomStudents(2, 10)])
+                .then((results) => {
+                  // does this make any difference? do we need both? is one deprecated?
+                  tmp.courseAdmin = results[0];
+                  tmp.teachers = tmp.teachers.concat(results[0]);
+                  tmp.students = tmp.students.concat(results[1]);
+                  return tmp;
+                })
+                .then(() => new courseFixtures.Model(tmp).save());
+            })
           )
         )
       )
     )
-    .then(() => Course.find())
+    .then((neastedCourseList) => {
+      // unwind neasted list
+      const courses: ICourseModel[] = [];
+      neastedCourseList.map((courseList) =>
+        courseList.map((course) =>
+          courses.push(<ICourseModel>course))
+      );
+      return courses;
+    })
     .then((courses: ICourseModel[]) =>
       Promise.all(courses.map((course: ICourseModel, courseIndex: number) => {
         const courseLectures: IFixture = this.lectures[courseIndex];
@@ -108,20 +168,64 @@ export class FixtureLoader {
       }))
     );
   }
-}
 
-/*
-          const lect: ILectureModel = <ILectureModel> new courseLectures.Model(lecture);
-          const lectureUnits: IFixture[] = this.units[courseIndex][lectureIndex];
-          // saving Units
-          return Promise.all(lectureUnits.map((unitFixtures) =>
-            Promise.all(unitFixtures.data.map((unit) =>
-              new unitFixtures.Model(unit).save()
-            ))
-            // push units to lecture
-            .then((savedUnits: IUnitModel[]) => {
-              lect.units = lect.units.concat(savedUnits);
-            })
-          ))
-          .then(() => lect.save);
- */
+  getRandomTeacher() {
+    return this.getTeacher()
+      .then((teacher: IUserModel[]) => {
+        return teacher[this.getRandomNumber(0, teacher.length)];
+      });
+  }
+
+  getRandomStudent() {
+    return this.getStudents()
+      .then((students: IUserModel[]) => {
+        return students[this.getRandomNumber(0, students.length)];
+      });
+  }
+
+  getRandomStudents(min: number, max: number) {
+    return this.getStudents()
+      .then((students: IUserModel[]) => {
+        const shuffeledStudents = this.shuffleArray(students);
+        const count = this.getRandomNumber(min, max);
+        const start = this.getRandomNumber(0, shuffeledStudents.length - count);
+        return shuffeledStudents.slice(start, start + count);
+      });
+  }
+
+  getTeacher() {
+    return this.getUser('teacher');
+  }
+
+  getStudents() {
+    return this.getUser('student');
+  }
+
+  getUser(role: string) {
+    if (isNullOrUndefined(role)) {
+      return User.find();
+    }
+    return User.find({role:  role});
+  }
+
+  getRandomNumber(start: number, end: number): number {
+    return Math.floor(Math.random() * end) + start;
+  }
+
+  shuffleArray (array: Array<any>) {
+    let tmp;
+    let current;
+    let top = array.length;
+
+    if (top) {
+      while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    }
+
+    return array;
+  }
+}
