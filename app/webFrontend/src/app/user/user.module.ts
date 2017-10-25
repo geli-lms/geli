3f70e172c2a1c5f811d223a5bf88035a73c35b7b
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2017-10-25 20:15:23 +0200

    Remove last of MaterialModule

--- app/webFrontend/src/app/user/user.module.ts
+++ app/webFrontend/src/app/user/user.module.ts
@@ -3,14 +3,14 @@ import {CommonModule} from '@angular/common';
 import {UserRoutingModule} from './user-routing.module';
 import {UserDetailsComponent} from './user-details/user-details.component';
 import {UserEditComponent} from './user-edit/user-edit.component';
-import {MaterialModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    MaterialModule,
+    SharedModule,
     ReactiveFormsModule,
     UserRoutingModule,
   ],
