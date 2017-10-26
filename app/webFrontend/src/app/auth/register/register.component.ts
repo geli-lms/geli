dff7142db6755b826895062f9fd3959f93acf0f7
AlexWeinfurter <alexander_weinfurter@hotmail.de>
2017-10-26 11:12:11 +0200

    Adds case insensitivity to login and registration

--- app/webFrontend/src/app/auth/register/register.component.ts
+++ app/webFrontend/src/app/auth/register/register.component.ts
@@ -35,7 +35,7 @@ export class RegisterComponent implements OnInit {
     if (this.registerForm.value.role !== 'student') {
       delete this.registerForm.value.uid;
     }
-    this.registerForm.value.email = this.registerForm.value.email.replace(/\s/g, '');
+    this.registerForm.value.email = this.registerForm.value.email.replace(/\s/g, '').toLowerCase();
     this.authenticationService.register(this.registerForm.value).then(
       (val) => {
         this.showProgress.toggleLoadingGlobal(false);
