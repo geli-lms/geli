af502aeee2242fd63aa70290e1944e5d959cd6b9
Ken Hasenbank <Ken.Hasenbank@stud.h-da.de>
2017-11-01 21:53:35 +0100

    add Info-Dialog

--- app/webFrontend/src/app/shared/services/dialog.service.ts
+++ app/webFrontend/src/app/shared/services/dialog.service.ts
@@ -4,11 +4,22 @@ import {MatDialog, MatDialogRef} from '@angular/material';
 import {ConfirmDialog} from '../components/confirm-dialog/confirm-dialog.component';
 import {IUser} from '../../../../../../shared/models/IUser';
 import {UploadDialog} from '../components/upload-dialog/upload-dialog.component';
+import {InfoDialog} from "../components/info-dialog/info-dialog.component";
 
 @Injectable()
 export class DialogService {
 
   constructor(private dialog: MatDialog) {
+  }
+
+  public info(title: string, message: string): Observable<boolean> {
+    let dialogRef: MdDialogRef<InfoDialog>;
+
+    dialogRef = this.dialog.open(ConfirmDialog);
+    dialogRef.componentInstance.title = title;
+    dialogRef.componentInstance.message = message;
+
+    return dialogRef.afterClosed();
   }
 
   public confirm(title: string, message: string, confirmText: string = 'Confirm'): Observable<boolean> {
