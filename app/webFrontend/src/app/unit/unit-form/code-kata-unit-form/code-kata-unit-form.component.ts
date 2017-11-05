2eaf0809a7c6cccbb3c3fa99d9afb11caad6051a
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-11-05 20:00:44 +0100

    add some more comment hints

--- app/webFrontend/src/app/unit/unit-form/code-kata-unit-form/code-kata-unit-form.component.ts
+++ app/webFrontend/src/app/unit/unit-form/code-kata-unit-form/code-kata-unit-form.component.ts
@@ -63,9 +63,9 @@ export class CodeKataUnitFormComponent implements OnInit {
       this.model = new CodeKataUnit(this.course._id);
       this.model.code =
         this.example.definition
-        + '\n\n' + this.areaSeperator + ((this.example.code.startsWith('//')) ? '\n' : '\n\n')
+        + '\n\n' + this.areaSeperator + ' code-section' + ((this.example.code.startsWith('//')) ? '\n' : '\n\n')
         + this.example.code
-        + '\n\n' + this.areaSeperator + ((this.example.test.startsWith('//')) ? '\n' : '\n\n')
+        + '\n\n' + this.areaSeperator + ' test-section' + ((this.example.test.startsWith('//')) ? '\n' : '\n\n')
         + this.example.test;
       this.model.definition = undefined;
       this.model.test = undefined;
