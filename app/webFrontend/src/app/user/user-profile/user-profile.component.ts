b3c1f2ff3b4a7dd5b78313441b985fa94c5dd796
Henrik <henrik.ochs@arcor.de>
2017-11-09 13:01:47 +0100

    The PAssowerds field now hide for an Admin in the Edit-User Section.

--- app/webFrontend/src/app/user/user-profile/user-profile.component.ts
+++ app/webFrontend/src/app/user/user-profile/user-profile.component.ts
@@ -35,6 +35,4 @@ export class UserProfileComponent implements OnInit {
     return (this.userService.isAdmin() || this.userService.isTeacher() ||
       this.userService.user._id === this.user._id) && this.user.uid;
   }
-
-
 }
