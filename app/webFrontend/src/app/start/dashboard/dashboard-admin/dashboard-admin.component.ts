e8eb07d470267ed2755e77c6ec8344d94b6afceb
PatrickSkowronek <patrick.skowronek@stud.h-da.de>
2017-11-14 14:01:13 +0100

    Fixed title in teacher dashboard Removed containers on admin dashboard

--- app/webFrontend/src/app/start/dashboard/dashboard-admin/dashboard-admin.component.ts
+++ app/webFrontend/src/app/start/dashboard/dashboard-admin/dashboard-admin.component.ts
@@ -14,10 +14,6 @@ export class DashboardAdminComponent implements OnInit {
   @Input()
   allCourses: ICourse[];
 
-  myCourses: ICourse[];
-  availableCourses: ICourse[];
-  furtherCourses: ICourse[];
-
   @Output()
   onEnroll = new EventEmitter();
 
@@ -25,42 +21,9 @@ export class DashboardAdminComponent implements OnInit {
               private router: Router,
               private dialog: MatDialog,
               private snackBar: MatSnackBar) {
-    this.myCourses = [];
-    this.furtherCourses = [];
-    this.availableCourses = [];
   }
 
   ngOnInit() {
-
-  }
-
-  ngOnChanges() {
-    this.sortCourses();
-  }
-
-  sortCourses() {
-    this.myCourses = [];
-    this.furtherCourses = [];
-    this.availableCourses = [];
-
-
-    for (const course of this.allCourses) {
-      if (this.filterAdminCourses(course)) {
-        this.myCourses.push(course);
-      } else if (this.filterMyCourses(course)) {
-        this.furtherCourses.push(course);
-      } else {
-        this.availableCourses.push(course);
-      }
-    }
-  }
-
-  filterAdminCourses(course: ICourse) {
-    return (course.courseAdmin._id === this.userService.user._id);
-  }
-
-  filterMyCourses(course: ICourse) {
-    return course.teachers.filter(teacher => teacher._id === this.userService.user._id);
   }
 
   enrollCallback() {
