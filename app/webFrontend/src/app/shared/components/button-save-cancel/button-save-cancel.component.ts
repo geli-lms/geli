4ee6231ca3b952365db6ee98dc1bfab08357e153
Henrik <henrik.ochs@arcor.de>
2017-11-21 14:47:44 +0100

    fixed linting, forget to push the new component.

---
+++ app/webFrontend/src/app/shared/components/button-save-cancel/button-save-cancel.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewEncapsulation, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button-save-cancel',
+  templateUrl: './button-save-cancel.component.html',
+  styleUrls: ['./button-save-cancel.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ButtonSaveCancelComponent implements OnInit {
+
+  @Output()
+  save: EventEmitter<any> = new EventEmitter();
+  @Output()
+  cancel: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  emitSave(): void {
+    this.save.emit(null);
+  }
+
+  emitCancel(): void {
+    this.cancel.emit(null);
+  }
+}
+
+
