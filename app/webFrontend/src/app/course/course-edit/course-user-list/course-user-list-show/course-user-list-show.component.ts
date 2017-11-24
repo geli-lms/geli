3c913165ea35f57380d345ac0084ad05008d96f3
Oliver Neff <oliver.neff@stud.h-da.de>
2017-11-24 19:16:31 +0100

    Add show user component

---
+++ app/webFrontend/src/app/course/course-edit/course-user-list/course-user-list-show/course-user-list-show.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-course-user-list-show',
+  templateUrl: './course-user-list-show.component.html',
+  styleUrls: ['./course-user-list-show.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CourseUserListShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
