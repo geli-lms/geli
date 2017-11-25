b6d92a8195708c24aab20555a3fb32acb35c9290
AlexWeinfurter <alexander_weinfurter@hotmail.de>
2017-11-25 23:01:51 +0100

    Implements duplication for courses

--- app/webFrontend/src/app/course/course-edit/course-edit.component.ts
+++ app/webFrontend/src/app/course/course-edit/course-edit.component.ts
@@ -1,13 +1,14 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CourseService, ExportService} from '../../shared/services/data.service';
+import {CourseService, DuplicationService, ExportService} from '../../shared/services/data.service';
 import {MatSnackBar} from '@angular/material';
 import {ShowProgressService} from '../../shared/services/show-progress.service';
 import {FileUploader} from 'ng2-file-upload';
 import {SaveFileService} from '../../shared/services/save-file.service';
 import {ICourse} from '../../../../../../shared/models/ICourse';
 import {ENROLL_TYPES, ENROLL_TYPE_WHITELIST, ENROLL_TYPE_FREE, ENROLL_TYPE_ACCESSKEY} from '../../../../../../shared/models/ICourse';
+import {UserService} from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-course-edit',
@@ -43,7 +44,9 @@ export class CourseEditComponent implements OnInit {
               private ref: ChangeDetectorRef,
               private exportService: ExportService,
               private saveFileService: SaveFileService,
-              private showProgress: ShowProgressService) {
+              private showProgress: ShowProgressService,
+              private duplicationService: DuplicationService,
+              private userService: UserService) {
 
     this.route.params.subscribe(params => {
       this.id = params['id'];
@@ -126,6 +129,14 @@ export class CourseEditComponent implements OnInit {
       this.saveFileService.save(this.courseOb.name, JSON.stringify(courseJSON, null, 2));
     } catch (err) {
       this.snackBar.open('Export course failed ' + err.json().message, 'Dismiss');
+    }
+  }
+
+  async onDuplicate() {
+    try {
+      const course = await this.duplicationService.duplicateCourse(this.courseOb, this.userService.user);
+    } catch (err) {
+      this.snackBar.open('Duplication of the course failed ' + err.json().message, 'Dismiss');
     }
   }
 
