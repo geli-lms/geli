ee581ca48713f3644a770a2e4b486af6cfa64bf2
micpah <michael.pahlke@stud.h-da.de>
2017-11-25 15:01:09 +0100

    Make code cleaner

--- app/webFrontend/src/app/unit/unit-form/task-unit-edit/task-unit-edit.component.ts
+++ app/webFrontend/src/app/unit/unit-form/task-unit-edit/task-unit-edit.component.ts
@@ -66,17 +66,19 @@ export class TaskUnitEditComponent implements OnInit {
     if (this.isTaskUnitValid()) {
       let taskPromise = null;
       if (this.add) {
-        taskPromise = this.unitService.addTaskUnit(this.model, this.lectureId)
+        taskPromise = this.unitService.addTaskUnit(this.model, this.lectureId);
       } else {
         taskPromise = this.unitService.updateTaskUnit(this.model);
       }
       taskPromise.then(
         (task) => {
-          this.snackBar.open(`Task ${this.add ? 'created' : 'updated'}`, '', {duration: 3000});
+          const message = `Task ${this.add ? 'created' : 'updated'}`;
+          this.snackBar.open(message, '', {duration: 3000});
           this.onDone();
         },
         (error) => {
-          this.snackBar.open(`Couldn\'t ${this.add ? 'create' : 'update'} task`, '', {duration: 3000});
+          const message = `Couldn\'t ${this.add ? 'create' : 'update'} task`;
+          this.snackBar.open(message, '', {duration: 3000});
         });
     }
   };
@@ -85,20 +87,26 @@ export class TaskUnitEditComponent implements OnInit {
     const taskUnit = this.model;
 
     if (taskUnit.name === null || taskUnit.name.trim() === '') {
-      this.snackBar.open('Task not valid: Name is required', '', {duration: 3000});
+      const message = 'Task not valid: Name is required';
+      this.snackBar.open(message, '', {duration: 3000});
       return false
     } else if (taskUnit.tasks.length === 0) {
-      this.snackBar.open('Task not valid: At least one question is required', '', {duration: 3000});
+      const message = 'Task not valid: At least one question is required';
+      this.snackBar.open(message, '', {duration: 3000});
       return false
     } else {
+      // Check if all tasks i.e. questions are valid
       for (const task of taskUnit.tasks) {
         if (task.name === undefined || task.name.trim() === '') {
-          this.snackBar.open('Task not valid: Every question requires some text', '', {duration: 3000});
+          const message = 'Task not valid: Every question requires some text';
+          this.snackBar.open(message, '', {duration: 3000});
           return false
         } else if (task.answers.length < 2) {
-          this.snackBar.open('Task not valid: Every question requires at least two answers', '', {duration: 3000});
+          const message = 'Task not valid: Every question requires at least two answers';
+          this.snackBar.open(message, '', {duration: 3000});
           return false
         } else {
+          // Check if answers are valid
           let noAnswersChecked = true;
 
           for (const answer of task.answers) {
@@ -106,12 +114,14 @@ export class TaskUnitEditComponent implements OnInit {
               noAnswersChecked = !answer.value
             }
             if (answer.text === undefined || answer.text.trim() === '') {
-              this.snackBar.open('Task not valid: Every answer requires some text', '', {duration: 3000});
+              const message = 'Task not valid: Every answer requires some text';
+              this.snackBar.open(message, '', {duration: 3000});
               return false
             }
           }
           if (noAnswersChecked) {
-            this.snackBar.open('Task not valid: Every question requires at least one checked answer', '', {duration: 3000});
+            const message = 'Task not valid: Every question requires at least one checked answer';
+            this.snackBar.open(message, '', {duration: 3000});
             return false
           }
         }
@@ -130,15 +140,6 @@ export class TaskUnitEditComponent implements OnInit {
 
   removeLastAnswer(task: any) {
     task.answers.pop();
-  }
-
-  updateTask(task: any) {
-    this.taskService.updateItem(task).then(
-      (val) => {
-        this.snackBar.open('Task saved', 'Update', {duration: 2000});
-      }, (error) => {
-        // THIS METHOD WILL BE REMOVED ANYWAY
-      });
   }
 
   removeTask(task: any) {
