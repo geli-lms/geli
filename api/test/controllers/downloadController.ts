a7a8a935bccb56e214a71c536629042ba5c3ccca
Patrick Skowronek <patrick.skowronek@stud.h-da.de>
2017-11-28 14:02:58 +0100

    Revert tests

--- api/test/controllers/downloadController.ts
+++ api/test/controllers/downloadController.ts
@@ -21,31 +21,6 @@ describe('DownloadController', () => {
   beforeEach(() => fixtureLoader.load());
 
   describe(`POST ${BASE_URL}`, () => {
-    it('It should post a download object and get a link to a zip file', async () => {
-      const user = await User.findOne({email: 'teacher1@test.local'});
-      const course = await Course.findOne({name: 'Introduction to web development'});
-      const lecture = await Lecture.findOne({name: 'Coding Train'});
-      const downloadObject = {
-        course: course.name,
-        letcture: lecture.name,
-        units: lecture.units
-      };
-
-      return new Promise((resolve, reject) => {
-        chai.request(app)
-          .post(BASE_URL)
-          .set('Authorization', `JWT ${JwtUtils.generateToken(user)}`)
-          .send(downloadObject)
-          .end((err, res) => {
-            if (err) {
-              return reject(err);
-            }
-
-            res.status.should.be.equal(200);
-            resolve();
-          });
-      });
-    });
 
     it('It should fail: Authorization', async () => {
       const user = await User.findOne({email: 'teacher1@test.local'});
@@ -65,58 +40,6 @@ describe('DownloadController', () => {
             if (err) {
               res.status.should.be.equal(401);
               err.message.should.be.equal('Unauthorized');
-              resolve();
-            }
-          });
-      });
-    });
-
-    it('It should fail: wrong id', async () => {
-      const user = await User.findOne({email: 'teacher1@test.local'});
-      const course = await Course.findOne({name: 'Introduction to web development'});
-      const lecture = await Lecture.findOne({name: 'Coding Train'});
-
-      const downloadObject = {
-        course: course.name,
-        letcture: lecture.name,
-        units: '000000000000000000000000'
-      };
-
-      return new Promise((resolve, reject) => {
-        chai.request(app)
-          .post(BASE_URL)
-          .set('Authorization', `JWT ${JwtUtils.generateToken(user)}`)
-          .send(downloadObject)
-          .end((err, res) => {
-            if (err) {
-              res.status.should.be.equal(500);
-              err.message.should.be.equal('Internal Server Error');
-              resolve();
-            }
-          });
-      });
-    });
-
-    it('It should fail: empty array', async () => {
-      const user = await User.findOne({email: 'teacher1@test.local'});
-      const course = await Course.findOne({name: 'Introduction to web development'});
-      const lecture = await Lecture.findOne({name: 'Coding Train'});
-
-      const downloadObject = {
-        course: course.name,
-        letcture: lecture.name,
-        units: Array
-      };
-
-      return new Promise((resolve, reject) => {
-        chai.request(app)
-          .post(BASE_URL)
-          .set('Authorization', `JWT ${JwtUtils.generateToken(user)}`)
-          .send(downloadObject)
-          .end((err, res) => {
-            if (err) {
-              res.status.should.be.equal(500);
-              err.message.should.be.equal('Internal Server Error');
               resolve();
             }
           });
