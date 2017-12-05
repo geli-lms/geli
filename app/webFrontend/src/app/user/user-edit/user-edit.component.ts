c06d8c35b18ddb0f25dfa76fd9c3a67da45116d7
Ken Hasenbank <Ken.Hasenbank@stud.h-da.de>
2017-12-05 17:48:14 +0100

    refactor async/await

--- app/webFrontend/src/app/user/user-edit/user-edit.component.ts
+++ app/webFrontend/src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {MatSnackBar} from '@angular/material';
@@ -30,17 +30,16 @@ export class UserEditComponent implements OnInit {
               private formBuilder: FormBuilder,
               public dialogService: DialogService,
               public snackBar: MatSnackBar,
-              private titleService: TitleService) {
+              private titleService: TitleService,
+              private cdRef: ChangeDetectorRef,) {
     this.generateForm();
   }
 
   showPwFields() {
     if (this.user) {
-      if (this.userService.user._id === this.user._id) {
-        return true;
-      }
-      return false;
+      return this.userService.user._id === this.user._id;
     }
+    return false;
   }
 
   ngOnInit() {
@@ -54,22 +53,22 @@ export class UserEditComponent implements OnInit {
     this.getUserData();
   }
 
-  getUserData() {
-    this.userDataService.readSingleItem(this.id).then(
-      (val: any) => {
-        this.user = val;
-        this.userForm.patchValue({
-          profile: {
-            firstName: this.user.profile.firstName,
-            lastName: this.user.profile.lastName
-          },
-          email: this.user.email,
-        });
-        this.titleService.setTitleCut(['Edit User: ', this.user.profile.firstName]);
-      },
-      (error) => {
-        this.snackBar.open(error, 'Dismiss');
+  async getUserData() {
+    try {
+      const user = await this.userDataService.readSingleItem(this.id);
+      this.user = <IUser>user;
+      this.userForm.patchValue({
+        profile: {
+          firstName: this.user.profile.firstName,
+          lastName: this.user.profile.lastName
+        },
+        email: this.user.email,
       });
+      this.titleService.setTitleCut(['Edit User: ', this.user.profile.firstName]);
+    } catch (error) {
+      this.snackBar.open(error.json().message, 'Dismiss');
+    }
+    this.cdRef.detectChanges();
   }
 
   onSubmit() {
@@ -99,23 +98,22 @@ export class UserEditComponent implements OnInit {
     return saveIUser;
   }
 
-  updateUser() {
+  async updateUser() {
     this.showProgress.toggleLoadingGlobal(true);
-    this.userDataService.updateItem(this.user).then(
-      (val) => {
-        this.showProgress.toggleLoadingGlobal(false);
-        this.snackBar.open('Profile successfully updated.', '', {duration: 3000});
+    try {
+      const user = await this.userDataService.updateItem(this.user);
+      this.snackBar.open('Profile successfully updated.', '', {duration: 3000});
 
-        if (this.userService.isLoggedInUser(val)) {
-          this.userService.setUser(val);
-        }
-        this.navigateBack();
-      },
-      (error) => {
-        this.showProgress.toggleLoadingGlobal(false);
-        this.snackBar.open(error.json().message, 'Dismiss');
-      });
+      if (this.userService.isLoggedInUser(user)) {
+        this.userService.setUser(user);
+      }
+      this.navigateBack();
+    } catch (error) {
+      this.snackBar.open(error.json().message, 'Dismiss');
+    }
+    this.showProgress.toggleLoadingGlobal(false);
   }
+
 
   generateForm() {
     this.userForm = this.formBuilder.group({
@@ -129,40 +127,34 @@ export class UserEditComponent implements OnInit {
     });
   }
 
-  resetPassword() {
-    this.dialogService
-      .confirmUpdate('property', 'password', this.user.email)
-      .subscribe(res => {
-        if (res) {
-          this.user.password = this.generatePass(12);
-          this.showProgress.toggleLoadingGlobal(true);
-          this.userDataService.updateItem(this.user).then(
-            (val) => {
-              this.showProgress.toggleLoadingGlobal(false);
-              this.dialogService.info(
-                'Password successfully updated',
-                'Password for user ' + this.user.email + ' was updated to: \'' + this.user.password + '\'')
-                .subscribe(() => this.snackBar.open('Password updated', '', {duration: 3000}));
-            },
-            (error) => {
-              this.showProgress.toggleLoadingGlobal(false);
-              this.snackBar.open(JSON.parse(error._body).message, '', {duration: 3000});
-            }
-          );
-        }
-      });
+  async resetPassword() {
+    const confirm = await this.dialogService.confirmUpdate('property', 'password', this.user.email).toPromise();
+    if (!confirm) {
+      return false;
+    }
+    this.user.password = this.generatePass(12);
+    this.showProgress.toggleLoadingGlobal(true);
+    const user = await this.userDataService.updateItem(this.user);
+    try {
+      if (!user) {
+        throw new Error('an unknown error occured');
+      }
+      await this.dialogService.info(
+        'Password successfully updated',
+        'Password for user ' + this.user.email + ' was updated to: \'' + this.user.password + '\'').toPromise();
+      this.snackBar.open('Password updated', '', {duration: 3000});
+    } catch (error) {
+      this.snackBar.open(error.json().message, '', {duration: 3000});
+    }
+    this.showProgress.toggleLoadingGlobal(false);
   }
 
-  openAddPictureDialog() {
-    this.dialogService.upload(this.user)
-      .subscribe((response) => {
-      if (response) {
-        if (response.success) {
-          this.userService.setUser(response.user);
-          this.snackBar.open('User image successfully uploaded.', '', {duration: 3000});
-        }
-      }
-    });
+  async openAddPictureDialog() {
+    const response = await this.dialogService.upload(this.user).toPromise();
+    if (response && response.success && response.user) {
+      this.userService.setUser(response.user);
+      this.snackBar.open('User image successfully uploaded.', '', {duration: 3000});
+    }
   }
 
   private navigateBack() {
