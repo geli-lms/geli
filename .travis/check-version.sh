74712d299eff9a72a5fc521e8b88c75c70ba3bac
Alexander Eimer <alexander.eimer@gmail.com>
2017-12-05 14:51:01 +0100

    Now checking api and frontend for correct version

--- .travis/check-version.sh
+++ .travis/check-version.sh
@@ -7,6 +7,8 @@ IPWD="$(pwd)"
 # Import shared vars
 . ${DIR}/_shared-vars.sh
 
+PATHS_TO_CHECK=("api" "app/webFrontend")
+
 echo
 echo "+++ Check if current version already used"
 echo
@@ -23,25 +25,32 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
             | awk -F: '{print $3}' \
             )
 
-        PACKAGE_VERSION=$(cat api/package.json \
-            | grep version \
-            | head -1 \
-            | awk -F: '{ print $2 }' \
-            | sed 's/[",]//g' \
-            )
+        for CUR_PATH in "${PATHS_TO_CHECK[@]}"
+        do
+            echo "+ checking '$CUR_PATH/package.json'"
 
-        while read -r line; do
-            # Check if line beginns with a "v"
-            if [[ $line == v* ]]; then
-                semverEQ "$line" "$PACKAGE_VERSION"
-                
-                # Check if same
-                if [[ $? == 0 ]]; then
-                    echo -e "${RED}+ ERROR: Version '$line' already exists on DockerHub${NC}"
-                    exit 1
+            PACKAGE_VERSION=$(cat "$CUR_PATH/package.json" \
+                | grep version \
+                | head -1 \
+                | awk -F: '{ print $2 }' \
+                | sed 's/[",]//g' \
+                )
+
+            while read -r LINE; do
+                # Check if line beginns with a "v"
+                if [[ $LINE == v* ]]; then
+                    semverEQ "$LINE" "$PACKAGE_VERSION"
+                    
+                    # Check if same
+                    if [[ $? == 0 ]]; then
+                        echo -e "${RED}+ ERROR: Version '$LINE' already exists on DockerHub${NC}"
+                        exit 1
+                    fi
                 fi
-            fi
-        done <<< "$EXISTING_TAGS"
+            done <<< "$EXISTING_TAGS"
+
+            echo -e "${GREEN}+ package.json is ok${NC}"
+        done
 
         echo -e "${GREEN}+ Everything is fine, have a great day :)${NC}"
     else
