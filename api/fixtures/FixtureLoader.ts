83e8223679e72da230fc7485359220832ef86a20
Ken Hasenbank <Hasenbank.Ken@Gmail.com>
2017-12-12 17:27:22 +0100

    refactor fixtureLoader
    to load all json fixtures in '/courses' and '/users'

--- api/fixtures/FixtureLoader.ts
+++ api/fixtures/FixtureLoader.ts
@@ -1,89 +1,15 @@
 import * as mongoose from 'mongoose';
 import config from '../src/config/main';
-
-import {studentFixtures} from './UserFixtures/studentFixtures';
-import {courseFixtures} from './courseFixtures';
-import {dumpTestLectureFixtures} from './lectureFixtures/1-dumpTest-lectureFixtures';
-import {IFixture} from './IFixture';
-import {ICourseModel} from '../src/models/Course';
-import {ILectureModel} from '../src/models/Lecture';
-import {adminFixtures} from './UserFixtures/adminFixtures';
-import {teacherFixtures} from './UserFixtures/teacherFixtures';
-import {tutorFixtures} from './UserFixtures/tutorFixtures';
-import {webDevelopmentLectureFixtures} from './lectureFixtures/2-intWebDev-lectureFixtures';
-import {dummyFreeTextFixture} from './unitFixtures/dummyFreeTextFixture';
-import {randomFreeTextFixture} from './unitFixtures/randomFreeTextFixture';
-import {simpleCodeKataFixture} from './unitFixtures/simpleCodeKataFixtures';
-import {IUnitModel} from '../src/models/units/Unit';
-import {advancedWebDevelopmentLectureFixtures} from './lectureFixtures/3-advWebDev-lectureFixtures';
-import {computerGraphicsLectureFixtures} from './lectureFixtures/4-comGra-lectureFixtures';
-import {randomLectureFixtures} from './lectureFixtures/5-randomness-lectureFixtures';
-import {mpseLectureFixtures} from './lectureFixtures/6-MPSE-lectureFixtures';
 import {IUserModel, User} from '../src/models/User';
 import {isNullOrUndefined} from 'util';
-import {loremFreeTextFixture} from './unitFixtures/loremFreeTextFixture';
-import {readmeFreeTextFixture} from './unitFixtures/readmeFreeTextFixture';
-import {mpseIntFreeTextFixture} from './unitFixtures/mpseIntFreeTextFixture';
-import {studentFreeTextFixture} from './unitFixtures/studentFreeTextFixture';
-import {hardCodeKataFixture} from './unitFixtures/hardCodeKataFixtures';
-import {studentCodeKataFixture} from './unitFixtures/studentCodeKataFixtures';
+import * as fs from 'fs';
+import {Course} from '../src/models/Course';
+import {IUser} from '../../shared/models/IUser';
+import * as crypto from 'crypto';
 
 export class FixtureLoader {
-
-  private user: Array<IFixture> = [
-    adminFixtures,
-    teacherFixtures,
-    tutorFixtures,
-    studentFixtures,
-  ];
-
-  private courses: Array<IFixture> = [
-    courseFixtures,
-  ];
-
-  private lectures: Array<IFixture> = [
-    /*course 1 lectures*/ dumpTestLectureFixtures,
-    /*course 2 lectures*/ webDevelopmentLectureFixtures,
-    /*course 3 lectures*/ advancedWebDevelopmentLectureFixtures,
-    /*course 4 lectures*/ computerGraphicsLectureFixtures,
-    /*course 5 lectures*/ randomLectureFixtures,
-    /*course 6 lectures*/ mpseLectureFixtures,
-  ];
-
-  private units = [
-    [ // dumpTestLectureFixtures
-      /*lecture 1 units*/ [loremFreeTextFixture],
-      /*lecture 2 units*/ [dummyFreeTextFixture, randomFreeTextFixture],
-      /*lecture 3 units*/ [dummyFreeTextFixture],
-    ],
-    [ // webDevelopmentLectureFixtures
-      /*lecture 1 units*/ [randomFreeTextFixture],
-      /*lecture 2 units*/ [simpleCodeKataFixture],
-      /*lecture 3 units*/ [dummyFreeTextFixture],
-    ],
-    [ // advancedWebDevelopmentLectureFixtures
-      /*lecture 1 units*/ [loremFreeTextFixture],
-      /*lecture 2 units*/ [hardCodeKataFixture],
-      /*lecture 3 units*/ [randomFreeTextFixture],
-    ],
-    [ // computerGraphicsLectureFixtures
-      /*lecture 1 units*/ [loremFreeTextFixture],
-      /*lecture 2 units*/ [dummyFreeTextFixture],
-      /*lecture 3 units*/ [randomFreeTextFixture],
-      /*lecture 4 units*/ [dummyFreeTextFixture],
-    ],
-    [ // randomLectureFixtures
-      /*lecture 1 units*/ [randomFreeTextFixture],
-      /*lecture 2 units*/ [randomFreeTextFixture],
-      /*lecture 3 units*/ [loremFreeTextFixture],
-      /*lecture 4 units*/ [randomFreeTextFixture],
-    ],
-    [ // mpseLectureFixtures
-      /*lecture 1 units*/ [mpseIntFreeTextFixture],
-      /*lecture 2 units*/ [readmeFreeTextFixture],
-      /*lecture 3 units*/ [studentFreeTextFixture, studentCodeKataFixture],
-    ],
-  ];
+  private usersDirectory = 'build/fixtures/users/';
+  private coursesDirectory = 'build/fixtures/courses/';
 
   constructor() {
     (<any>mongoose).Promise = global.Promise;
@@ -93,115 +19,142 @@ export class FixtureLoader {
     }
   }
 
-  load() {
-    return mongoose.connection.dropDatabase()
-    .then(() =>
-      // Load users to Database
-      Promise.all(
-        this.user.map((userFixtures) =>
-          Promise.all(
-            userFixtures.data.map((user) =>
-              new userFixtures.Model(user).save()
-            )
-          )
-        )
-      ))
-    .then(() =>
-      // Load courses
-      Promise.all(
-        this.courses.map((courseFixturesVar) =>
-          Promise.all(
-            courseFixturesVar.data.map((course) => {
-              // add random teacher as course admin
-              // add 2-10 random students
-              const tmp = <ICourseModel>course;
-              return Promise.all([this.getRandomTeacher(), this.getRandomStudents(2, 10)])
-                .then((results) => {
-                  // does this make any difference? do we need both? is one deprecated?
-                  tmp.courseAdmin = results[0];
-                  tmp.teachers = tmp.teachers.concat(results[0]);
-                  tmp.students = tmp.students.concat(results[1]);
-                  return tmp;
-                })
-                .then(() => new courseFixturesVar.Model(tmp).save());
-            })
-          )
-        )
-      )
-    )
-    .then((neastedCourseList) => {
-      // unwind neasted list
-      const courses: ICourseModel[] = [];
-      neastedCourseList.map((courseList) =>
-        courseList.map((course) =>
-          courses.push(<ICourseModel>course))
-      );
-      return courses;
-    })
-    .then((courses: ICourseModel[]) =>
-      Promise.all(courses.map((course: ICourseModel, courseIndex: number) => {
-        const courseLectures: IFixture = this.lectures[courseIndex];
-        // save all lectures
-        return Promise.all(courseLectures.data.map((lecture: ILectureModel, lectureIndex: number) =>  {
-          const lect: ILectureModel = <ILectureModel> new courseLectures.Model(lecture);
-          const lectureUnits: IFixture[] = this.units[courseIndex][lectureIndex];
+  async load() {
+    await mongoose.connection.dropDatabase();
+    const userfixtures = fs.readdirSync(this.usersDirectory);
+    const coursefixtures = fs.readdirSync(this.coursesDirectory);
 
-          return Promise.all(lectureUnits.map((unitFixtures) =>
-            // save Units
-            Promise.all(unitFixtures.data.map((unit: IUnitModel) => {
-              unit._course = course._id;
-              return new unitFixtures.Model(unit).save();
-            }))
-            // push units to lecture
-            .then((savedUnits: IUnitModel[]) => {
-              lect.units = lect.units.concat(savedUnits);
-              return savedUnits.length;
-            })
-          ))
-          .then(() => lect.save());
-        }))
-        // push lectures to course
-        .then((savedLectures: ILectureModel[]) => {
-          course.lectures = course.lectures.concat(savedLectures);
-          return course.save();
-        });
-      }))
-    );
-  }
+    // import userfiles
+    // order needs to be always the same for 'getRandom...(hash)' to work properly
+    userfixtures.forEach((userFile: string) => {
+      const file = fs.readFileSync(this.usersDirectory + userFile);
+      const users = JSON.parse(file.toString());
 
-  getRandomTeacher() {
-    return this.getTeacher()
-      .then((teacher: IUserModel[]) => {
-        return teacher[this.getRandomNumber(0, teacher.length)];
+      // each file consists of an array of users to provide possibility of logical grouping
+      users.forEach(async (userDef: IUser) => {
+        await new User(userDef).save();
       });
+    });
+
+    // import coursefiles
+    const courseIDs = await Promise.all(coursefixtures.map( async (courseFile: string) => {
+      const file = fs.readFileSync(this.coursesDirectory + courseFile);
+      const course = JSON.parse(file.toString());
+
+      const hash = crypto.createHash('sha1').update(file.toString()).digest('hex');
+
+      // assign random courseAdmin
+      const teacher = await this.getRandomTeacher(hash);
+      // assign random courseTeachers
+      course.teachers = await this.getRandomTeachers(0, 2, hash);
+      // enroll random array of Students
+      course.students = await this.getRandomStudents(2, 10, hash);
+
+      const importedCourse = await Course.schema.statics.importJSON(course, teacher, course.active);
+      return importedCourse._id;
+    }));
   }
 
-  getRandomStudent() {
-    return this.getStudents()
-      .then((students: IUserModel[]) => {
-        return students[this.getRandomNumber(0, students.length)];
-      });
+  // returns a random user with role 'admin'
+  // returns always the same admin when you provide the same hash (given the fixture base did not change)
+  async getRandomAdmin(hash?: string) {
+    if (hash) {
+      const admins = await this.getAdmins();
+      return admins[this.getNumberFromString(hash, 0, admins.length)];
+    } else {
+      const admins = await this.getAdmins();
+      return admins[this.getRandomNumber(0, admins.length)];
+    }
   }
 
-  getRandomStudents(min: number, max: number) {
-    return this.getStudents()
-      .then((students: IUserModel[]) => {
-        const shuffeledStudents = this.shuffleArray(students);
-        const count = this.getRandomNumber(min, max);
-        const start = this.getRandomNumber(0, shuffeledStudents.length - count);
-        return shuffeledStudents.slice(start, start + count);
-      });
+  // returns an array of random user with role 'admin'
+  // returns always the same array when you provide the same hash (given the fixture base did not change)
+  async getRandomAdmins(min: number, max: number, hash?: string) {
+    if (hash) {
+      const admins = await this.getAdmins();
+      const count = this.getNumberFromString(hash, min, max);
+      const start = this.getNumberFromString(hash, 0, admins.length - count);
+      return admins.slice(start, start + count);
+    } else {
+      const admins = await this.getAdmins();
+      const shuffeledAdmins = this.shuffleArray(admins);
+      const count = this.getRandomNumber(min, max);
+      const start = this.getRandomNumber(0, shuffeledAdmins.length - count);
+      return shuffeledAdmins.slice(start, start + count);
+    }
   }
 
-  getTeacher() {
+  // returns a random user with role 'teacher'
+  // returns always the same teacher when you provide the same hash (given the fixture base did not change)
+  async getRandomTeacher(hash?: string) {
+    if (hash) {
+      const teacher = await this.getTeacher();
+      return teacher[this.getNumberFromString(hash, 0, teacher.length)];
+    } else {
+      const teacher = await this.getTeacher();
+      return teacher[this.getRandomNumber(0, teacher.length)];
+    }
+  }
+
+  // returns an array of random user with role 'teacher'
+  // returns always the same array when you provide the same hash (given the fixture base did not change)
+  async getRandomTeachers(min: number, max: number, hash?: string) {
+    if (hash) {
+      const teacher = await this.getTeacher();
+      const count = this.getNumberFromString(hash, min, max);
+      const start = this.getNumberFromString(hash, 0, teacher.length - count);
+      return teacher.slice(start, start + count);
+    } else {
+      const teacher = await this.getTeacher();
+      const shuffeledTeacher = this.shuffleArray(teacher);
+      const count = this.getRandomNumber(min, max);
+      const start = this.getRandomNumber(0, shuffeledTeacher.length - count);
+      return shuffeledTeacher.slice(start, start + count);
+    }
+  }
+
+  // returns a random user with role 'student'
+  // returns always the same student when you provide the same hash (given the fixture base did not change)
+  async getRandomStudent(hash?: string) {
+    if (hash) {
+      const students = await this.getStudents();
+      return students[this.getNumberFromString(hash, 0, students.length)];
+    } else {
+      const students = await this.getStudents();
+      return students[this.getRandomNumber(0, students.length)];
+    }
+  }
+
+  // returns an array of random user with role 'student'
+  // returns always the same array when you provide the same hash (given the fixture base did not change)
+  async getRandomStudents(min: number, max: number, hash?: string) {
+    if (hash) {
+      const students = await this.getStudents();
+      const count = this.getNumberFromString(hash, min, max);
+      const start = this.getNumberFromString(hash, 0, students.length - count);
+      return students.slice(start, start + count);
+    } else {
+      const students = await this.getStudents();
+      const shuffeledStudents = this.shuffleArray(students);
+      const count = this.getRandomNumber(min, max);
+      const start = this.getRandomNumber(0, shuffeledStudents.length - count);
+      return shuffeledStudents.slice(start, start + count)
+    }
+  }
+
+  async getAdmins() {
+    return this.getUser('admin');
+  }
+
+  async getTeacher() {
     return this.getUser('teacher');
   }
 
-  getStudents() {
+  async getStudents() {
     return this.getUser('student');
   }
 
-  getUser(role: string) {
+  async getUser(role: string) {
     if (isNullOrUndefined(role)) {
       return User.find();
     }
@@ -210,6 +163,16 @@ export class FixtureLoader {
 
   getRandomNumber(start: number, end: number): number {
     return Math.floor(Math.random() * end) + start;
+  }
+
+  // returns a number for a given string between the boundaries of start(inclusive) and end(exclusive)
+  getNumberFromString(str: string, start: number, end: number): number {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash += char;
+    }
+    return start + (hash % (end - start));
   }
 
   shuffleArray (array: Array<any>) {
