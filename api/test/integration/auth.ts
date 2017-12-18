0c2ec5d8221d25a97584d5a9cc0833d54e259e70
Oliver Neff <oliver.neff@stud.h-da.de>
2017-12-19 00:13:09 +0100

    Implement test for authentication

--- api/test/integration/auth.ts
+++ api/test/integration/auth.ts
@@ -2,8 +2,12 @@ import * as chai from 'chai';
 import {Server} from '../../src/server';
 import {FixtureLoader} from '../../fixtures/FixtureLoader';
 import {User} from '../../src/models/User';
-import chaiHttp = require('chai-http');
 import * as errorCodes from '../../src/config/errorCodes'
+import {WhitelistUser} from '../../src/models/WhitelistUser';
+import {IUser} from '../../../shared/models/IUser';
+import {Course} from '../../src/models/Course';
+import {IWhitelistUser} from '../../../shared/models/IWhitelistUser';
+import chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 chai.should();
@@ -51,6 +55,63 @@ describe('Auth', () => {
         })
         .catch(done);
     });
+
+    it('should pass and enroll into course', (done) => {
+      const registerUser: IUser = new User();
+      registerUser.uid = '5468907';
+      registerUser.profile.firstName = 'firstName';
+      registerUser.profile.lastName = 'lastName';
+      registerUser.role = 'student';
+      registerUser.password = 'test1234';
+      registerUser.email = 'local@test.local.de';
+      WhitelistUser.create({
+        uid: registerUser.uid,
+        firstName: registerUser.profile.firstName,
+        lastName: registerUser.profile.lastName
+      }).then((whitelistUser: IWhitelistUser) => {
+        Course.create({
+          name: 'Test Course 1',
+          enrollType: 'whitelist',
+          whitelist: []
+        }).then((noElemCourse) => {
+          Course.create({
+            name: 'Test Course 2',
+            enrollType: 'whitelist',
+            whitelist: [whitelistUser]
+          }).then(elemCourse => {
+            chai.request(app)
+              .post(`${BASE_URL}/register`)
+              .send(registerUser)
+              .end(async (err, res) => {
+                res.status.should.be.equal(204);
+                // Get updated Course.
+                Course.findById(noElemCourse._id)
+                  .populate('whitelist')
+                  .populate('students')
+                  .then(
+                    noElemC => {
+                      noElemC.whitelist.length.should.be.equal(0);
+                      noElemC.students.length.should.be.equal(0);
+                      Course.findById(elemCourse._id)
+                        .populate('whitelist')
+                        .populate('students')
+                        .then(
+                          c => {
+                            c.whitelist.length.should.be.equal(1);
+                            c.students.length.should.be.equal(1);
+                            c.whitelist[0].uid.should.be.equal(c.students[0].uid);
+                            c.whitelist[0].firstName.should.be.equal(c.students[0].profile.firstName.toLowerCase());
+                            c.whitelist[0].lastName.should.be.equal(c.students[0].profile.lastName.toLowerCase());
+                            done();
+                          })
+                    }
+                  );
+              });
+          });
+        });
+      });
+    });
+
 
     it('should fail (registration as admin)', (done) => {
       User.findOne({email: 'teacher1@test.local'})
