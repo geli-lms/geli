db739b0f4ee101d9d5dc27594e85d779d13be16b
Lukas Korte <lukas.korte@incloud.de>
2017-12-21 15:17:09 +0100

    Fix conditional

--- .travis/sentry.sh
+++ .travis/sentry.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #if [ "$TRAVIS_PULL_REQUEST" == "false" ] && ([ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]); then
-if [[ "$TRAVIS_PULL_REQUEST" == "true" && ("$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" || "$TRAVIS_BRANCH" == "feature/427-Improve-Sentry-error-handling") ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "true" || ("$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" || "$TRAVIS_BRANCH" == "feature/427-Improve-Sentry-error-handling") ]]; then
     echo "Downloading and installing sentry-cli"
     export INSTALL_DIR=$(pwd)
     curl -sL https://sentry.io/get-cli/ | bash
