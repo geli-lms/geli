0b5b969c89b6ee18ecdcfdecb82f3e0c9e23e026
Oliver Neff <oliver.neff@stud.h-da.de>
2017-12-21 12:25:00 +0100

    Remove test environments

--- api/test/mocha.env.js
+++ api/test/mocha.env.js
@@ -1,7 +1,2 @@
 process.env.DB_NAME = 'test';
 process.env.NODE_ENV = 'test';
-
-process.env.MAILSMTPSERVER = 'smtp.ethereal.email';
-process.env.MAILSMTPPORT = '587';
-process.env.MAILUSER = 'x664qe6txbhpjjar@ethereal.email';
-process.env.MAILPASS = 'B8SecahPMwRTafeJMZ';
