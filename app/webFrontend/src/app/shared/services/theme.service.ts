629534706a457b5d6196bac008226d8d95fcc893
Andreas Schroll <andreas@schroll.co>
2018-01-13 19:42:00 +0100

    check theme

--- app/webFrontend/src/app/shared/services/theme.service.ts
+++ app/webFrontend/src/app/shared/services/theme.service.ts
@@ -3,7 +3,7 @@ import {Injectable, Renderer2, RendererFactory2} from '@angular/core';
 @Injectable()
 export class ThemeService {
   private renderer: Renderer2;
-  themes: string[]= ['default', 'night', 'auto'];
+  themes: string[] = ['default', 'night', 'auto'];
 
   constructor(private rendererFactory: RendererFactory2) {
     this.renderer = rendererFactory.createRenderer(null, null);
@@ -13,18 +13,20 @@ export class ThemeService {
     for (const removeTheme of this.themes) {
       this.renderer.removeClass(document.body, removeTheme + 'Theme');
     }
-    switch (theme) {
-      case 'auto':
-        const time = new Date();
-        if (time.getHours() >= 22 || time.getHours() <= 6) {
-          this.renderer.addClass(document.body, 'nightTheme');
-        }
-        break;
-      case 'default':
-        break;
-      default :
-        this.renderer.addClass(document.body, theme + 'Theme');
-        break;
+    if (this.themes.indexOf(theme) !== -1) {
+      switch (theme) {
+        case 'auto':
+          const time = new Date();
+          if (time.getHours() >= 22 || time.getHours() <= 6) {
+            this.renderer.addClass(document.body, 'nightTheme');
+          }
+          break;
+        case 'default':
+          break;
+        default :
+          this.renderer.addClass(document.body, theme + 'Theme');
+          break;
+      }
     }
   }
 }
