788690d6296a338699f774f32317eb4905103906
Steffen Großpersky <steffen.grosspersky@stud.h-da.de>
2018-01-21 16:06:41 +0100

    Fix report service for unit details

--- app/webFrontend/src/app/shared/services/data/report.service.ts
+++ app/webFrontend/src/app/shared/services/data/report.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {DataService} from '../data.service';
 import {BackendService} from '../backend.service';
-import {IUser} from '../../../../../../../shared/models/IUser';
 
 @Injectable()
 export class ReportService extends DataService {
@@ -28,8 +27,7 @@ export class ReportService extends DataService {
   getUnitDetailForCourse(courseId: string, unitId: string) {
     const originalApiPath = this.apiPath;
     this.apiPath += 'details/courses/' + courseId + '/units/';
-    // FIXME: Why do we expect an array?
-    const promise = this.readSingleItem<IUser[]>(unitId);
+    const promise = this.readSingleItem(unitId);
     this.apiPath = originalApiPath;
     return promise;
   }
