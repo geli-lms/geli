3b7fffc11b28359ea34a3e7df4d5e8656173e52c
Alexander Eimer <alexander.eimer@gmail.com>
2018-01-23 16:11:35 +0100

    Introduces apidoc to travis

---
+++ .travis/apidoc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Path to this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Path the script was called from
+IPWD="$(pwd)"
+# Import shared vars
+. ${DIR}/_shared-vars.sh
+
+echo
+echo "+++ Create and publish API-Doc +++"
+echo
+if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+    if [[ -z $GITHUB_TOKEN ]]; then
+        echo "${RED}+ ERROR: NO GITHUB_TOKEN ENVVAR DEFINED. GO TO TRAVIS-SETTINGS AND DEFINE ONE.${NC}"
+        exit 1
+    fi
+
+    echo "+ Generate API-Doc"
+    cd api
+    npm run generate:apidoc
+    echo "+ Publish API-Doc to h-da/geli-doc"
+    ls -lah ./ ./apidocs # for debug only
+    cd apidocs
+    git init
+    git checkout -b gh-pages
+    git add .
+    git -c user.name='Travis' -c user.email='travis@travis-ci.org' commit -m 'Travis build: $TRAVIS_BUILD_NUMBER'
+    git push -f -q https://micpah:$GITHUB_TOKEN@github.com/h-da/geli-docs master &>/dev/null
+  else
+    echo -e "${YELLOW}+ WARNING: pull request #$TRAVIS_PULL_REQUEST -> skipping api-doc${NC}";
+  fi
+else
+  echo -e "${YELLOW}+ WARNING: branch $TRAVIS_BRANCH is not whitelisted -> skipping api-doc${NC}";
+fi
