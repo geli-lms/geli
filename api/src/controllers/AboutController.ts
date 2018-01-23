5f51addcb2d909ee10929d6e8f76f3517720927f
micpah <michael.pahlke@stud.h-da.de>
2018-01-23 14:58:24 +0100

    Add dummy comment for an api route

--- api/src/controllers/AboutController.ts
+++ api/src/controllers/AboutController.ts
@@ -4,6 +4,33 @@ import * as fs from 'fs-extra';
 @JsonController('/about')
 export class AboutController {
 
+  /**
+   * @api {get} /user/:id Get User information and Date of Registration.
+   * @apiVersion 0.2.0
+   * @apiName GetUser
+   * @apiGroup User
+   *
+   * @apiParam {Number} id Users unique ID.
+   *
+   * @apiSuccess {String} firstname  Firstname of the User.
+   * @apiSuccess {String} lastname   Lastname of the User.
+   * @apiSuccess {Date}   registered Date of Registration.
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *     "firstname": "John",
+   *     "lastname": "Doe"
+   *     }
+   *
+   * @apiError UserNotFound The id of the User was not found.
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 404 Not Found
+   *     {
+   *     "error": "UserNotFound"
+   *     }
+   */
   @Get('/dependencies')
   getDependencies() {
     return fs.readJson('nlf-licenses.json')
