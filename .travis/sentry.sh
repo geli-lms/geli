feba82a319317ae3cee534aa69f0f049b85d3e15
Alexander Eimer <alexander.eimer@gmail.com>
2018-01-24 20:34:13 +0100

    Adds some echos to sentry.sh

--- .travis/sentry.sh
+++ .travis/sentry.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+# Path to this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Path the script was called from
+IPWD="$(pwd)"
+# Import shared vars
+. ${DIR}/_shared-vars.sh
+
+echo
+echo "+++ Sentry +++"
+echo
+
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && ("$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop") ]]; then
-    echo "Downloading and installing sentry-cli"
+    echo "+ Downloading and installing sentry-cli"
     export INSTALL_DIR=$(pwd)
     curl -sL https://sentry.io/get-cli/ | bash
 
-    echo "Creating Sentry release and uploading source maps"
+    echo "+ Creating Sentry release and uploading source maps"
     ./sentry-cli releases -o geli -p staging new $TRAVIS_COMMIT
     ./sentry-cli releases -o geli -p staging files $TRAVIS_COMMIT upload-sourcemaps app/webFrontend/dist
     ./sentry-cli releases -o geli -p staging files $TRAVIS_COMMIT upload-sourcemaps --url-prefix /usr/src/app api/build
+else
+	echo -e "${YELLOW}+ Skipping Sentry - not on develop or master${NC}"
 fi
