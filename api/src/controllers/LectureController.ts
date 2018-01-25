0ee430b02625ff748462997c111e3e17d6a2a281
micpah <michael.pahlke@stud.h-da.de>
2018-01-25 03:30:09 +0100

    Add comments for lecture api

--- api/src/controllers/LectureController.ts
+++ api/src/controllers/LectureController.ts
@@ -10,12 +10,43 @@ import {Course} from '../models/Course';
 @UseBefore(passportJwtMiddleware)
 export class LectureController {
 
+  /**
+   * @api {get} /api/lecture Request lecture
+   * @apiName GetLecture
+   * @apiGroup Lecture
+   *
+   * @apiParam {String} id Lecture ID.
+   *
+   * @apiSuccess {Lecture} lecture Lecture.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         TODO
+   *     }
+   */
   @Get('/:id')
   getLecture(@Param('id') id: string) {
     return Lecture.findById(id)
       .then((l) => l.toObject());
   }
 
+  /**
+   * @api {post} /api/lecture Add lecture
+   * @apiName PostLecture
+   * @apiGroup Lecture
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {Object} data New lecture data.
+   * @apiParam {Request} request Request.
+   *
+   * @apiSuccess {Lecture} lecture Added lecture.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         TODO
+   *     }
+   */
   @Authorized(['teacher', 'admin'])
   @Post('/')
   addLecture(@Body() data: any, @Req() request: Request) {
@@ -32,6 +63,23 @@ export class LectureController {
       .then(({course, lecture}) => lecture.toObject());
   }
 
+  /**
+   * @api {put} /api/lecture Update lecture
+   * @apiName PutLecture
+   * @apiGroup Lecture
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {String} id Lecture ID.
+   * @apiParam {ILecture} lecture New lecture data.
+   *
+   * @apiSuccess {Lecture} lecture Updated lecture.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         TODO
+   *     }
+   */
   @Authorized(['teacher', 'admin'])
   @Put('/:id')
   updateLecture(@Param('id') id: string, @Body() lecture: ILecture) {
@@ -39,6 +87,20 @@ export class LectureController {
       .then((l) => l.toObject());
   }
 
+  /**
+   * @api {delete} /api/lecture Delete lecture
+   * @apiName DeleteLecture
+   * @apiGroup Lecture
+   *
+   * @apiParam {String} id Lecture ID.
+   *
+   * @apiSuccess {Boolean} result Confirmation of deletion.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "result": true
+   *     }
+   */
   @Delete('/:id')
   deleteLecture(@Param('id') id: string) {
     return Course.update({}, {$pull: {lectures: id}})
