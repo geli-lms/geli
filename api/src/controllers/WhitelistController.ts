49de451e27cbfc5bd9fc32f69f82e0cf2d7bba12
micpah <michael.pahlke@stud.h-da.de>
2018-03-22 00:37:34 +0100

    Add comments for whitelist api

--- api/src/controllers/WhitelistController.ts
+++ api/src/controllers/WhitelistController.ts
@@ -18,6 +18,28 @@ function escapeRegex(text: string) {
 @UseBefore(passportJwtMiddleware)
 export class WitelistController {
 
+  /**
+   * @api {get} /api/whitelist/:id Request whitelist user
+   * @apiName GetWhitelistUser
+   * @apiGroup Whitelist
+   *
+   * @apiParam {String} id Whitelist user ID.
+   *
+   * @apiSuccess {WhitelistUser} whitelistUser Whitelist user.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "__v": 0,
+   *         "updatedAt": "2018-03-21T23:22:23.758Z",
+   *         "createdAt": "2018-03-21T23:22:23.758Z",
+   *         "_id": "5ab2e92fda32ac2ab0f04b78",
+   *         "firstName": "max",
+   *         "lastName": "mustermann",
+   *         "uid": "876543",
+   *         "courseId": {...},
+   *         "id": "5ab2e92fda32ac2ab0f04b78"
+   *     }
+   */
   @Get('/:id')
   getUser(@Param('id') id: string) {
     return WhitelistUser.findById(id)
@@ -27,6 +49,30 @@ export class WitelistController {
       });
   }
 
+  /**
+   * @api {post} /api/whitelist/ Add whitelist user
+   * @apiName PostWhitelistUser
+   * @apiGroup Whitelist
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {IWhitelistUser} whitelistUser New whitelist user.
+   *
+   * @apiSuccess {WhitelistUser} w Added whitelist user.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "__v": 0,
+   *         "updatedAt": "2018-03-21T23:22:23.758Z",
+   *         "createdAt": "2018-03-21T23:22:23.758Z",
+   *         "_id": "5ab2e92fda32ac2ab0f04b78",
+   *         "firstName": "max",
+   *         "lastName": "mustermann",
+   *         "uid": "876543",
+   *         "courseId": {...},
+   *         "id": "5ab2e92fda32ac2ab0f04b78"
+   *     }
+   */
   @Post('/')
   @Authorized(['teacher', 'admin'])
   addWhitelistUser(@Body() whitelistUser: IWhitelistUser) {
@@ -34,6 +80,31 @@ export class WitelistController {
       .then((w) => w.toObject());
   }
 
+  /**
+   * @api {put} /api/whitelist/:id Update whitelist user
+   * @apiName PutWhitelistUser
+   * @apiGroup Whitelist
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {String} id Whitelist user ID.
+   * @apiParam {IWhitelistUser} whitelistUser New whitelist user.
+   *
+   * @apiSuccess {WhitelistUser} w Updated whitelist user.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "__v": 0,
+   *         "updatedAt": "2018-03-21T23:24:56.758Z",
+   *         "createdAt": "2018-03-21T23:22:23.758Z",
+   *         "_id": "5ab2e92fda32ac2ab0f04b78",
+   *         "firstName": "maximilian",
+   *         "lastName": "mustermann",
+   *         "uid": "876543",
+   *         "courseId": {...},
+   *         "id": "5ab2e92fda32ac2ab0f04b78"
+   *     }
+   */
   @Put('/:id')
   @Authorized(['teacher', 'admin'])
   updateWhitelistUser(@Param('id') id: string, @Body() whitelistUser: IWhitelistUser) {
@@ -44,6 +115,34 @@ export class WitelistController {
       .then((w) => w ? w.toObject({}) : undefined);
   }
 
+  /**
+   * @api {delete} /api/whitelist/:id Delete whitelist user
+   * @apiName DeleteWhitelistUser
+   * @apiGroup Whitelist
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {String} id Whitelist user ID.
+   *
+   * @apiSuccess {Object} deletion Object with deleted whitelist user.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "$__": {...},
+   *         "isNew": false,
+   *         "_doc": {
+   *             "__v": 0,
+   *             "courseId": {...},
+   *             "uid": "876543",
+   *             "lastName": "mustermann",
+   *             "firstName": "max",
+   *             "createdAt": "2018-03-21T23:22:23.758Z",
+   *             "updatedAt": "2018-03-21T23:22:23.758Z",
+   *             "_id": {...}
+   *         },
+   *         "$init": true
+   *     }
+   */
   @Delete('/:id')
   @Authorized(['teacher', 'admin'])
   deleteWhitelistUser(@Param('id') id: string) {
