b6541a7c6e879ec620038e0a6d189432371f4931
micpah <michael.pahlke@stud.h-da.de>
2018-03-21 13:53:01 +0100

    Add comments for export api

--- api/src/controllers/ExportController.ts
+++ api/src/controllers/ExportController.ts
@@ -9,18 +9,81 @@ import {Unit} from '../models/units/Unit';
 @Authorized(['teacher', 'admin'])
 export class ExportController {
 
+  /**
+   * @api {get} /api/export/course/:id Export course
+   * @apiName GetExportCourse
+   * @apiGroup Export
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {String} id Course ID.
+   *
+   * @apiSuccess {Course} course Course for export.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "name": "Test 101",
+   *         "description": "Some course desc",
+   *         "enrollType": "whitelist",
+   *         "lectures": [{
+   *             "name": "Lecture One",
+   *             "description": "Some lecture desc",
+   *             "units": []
+   *         }],
+   *         "hasAccessKey": false
+   *     }
+   */
   @Get('/course/:id')
   async exportCourse(@Param('id') id: string) {
     const course = await Course.findById(id);
     return course.exportJSON();
   }
 
+  /**
+   * @api {get} /api/export/lecture/:id Export lecture
+   * @apiName GetExportLecture
+   * @apiGroup Export
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {String} id Lecture ID.
+   *
+   * @apiSuccess {Lecture} lecture Lecture for export.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "name": "Lecture One",
+   *         "description": "Some lecture desc",
+   *         "units": []
+   *     }
+   */
   @Get('/lecture/:id')
   async exportLecture(@Param('id') id: string) {
     const lecture = await Lecture.findById(id);
     return lecture.exportJSON();
   }
 
+  /**
+   * @api {get} /api/export/unit/:id Export unit
+   * @apiName GetExportUnit
+   * @apiGroup Export
+   * @apiPermission teacher
+   * @apiPermission admin
+   *
+   * @apiParam {String} id Unit ID.
+   *
+   * @apiSuccess {Unit} unit Unit for export.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     {
+   *         "progressable": false,
+   *         "weight": 0,
+   *         "name": "First unit",
+   *         "description": null,
+   *         "markdown": "Welcome, this is the start",
+   *         "__t": "free-text"
+   *     }
+   */
   @Get('/unit/:id')
   async exportUnit(@Param('id') id: string) {
     const unit = await Unit.findById(id);
