52311f792e05be2ecce1030f83ae9d0c588e197b
Daniel Kesselberg <mail@danielkesselberg.de>
2018-04-12 17:59:26 +0200

    Fix access to error message

--- app/webFrontend/src/app/unit/code-kata-unit/code-kata-unit.component.ts
+++ app/webFrontend/src/app/unit/code-kata-unit/code-kata-unit.component.ts
@@ -80,7 +80,7 @@ export class CodeKataComponent implements OnInit {
           this.progress = item;
         })
         .catch((err) => {
-          this.snackBar.open(`An error occurred: ${err.json().message}`, '', {duration: 3000});
+          this.snackBar.open(`An error occurred: ${err.error.message}`, '', {duration: 3000});
         });
     } else {
       this.progressService.updateItem(this.progress)
@@ -89,7 +89,7 @@ export class CodeKataComponent implements OnInit {
           this.progress = item;
         })
         .catch((err) => {
-          this.snackBar.open(`An error occurred: ${err.json().message}`, '', {duration: 3000})
+          this.snackBar.open(`An error occurred: ${err.error.message}`, '', {duration: 3000})
         });
     }
   }
