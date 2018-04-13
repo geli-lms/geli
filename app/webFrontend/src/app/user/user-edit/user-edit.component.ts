c0b1c3a219356eb0ce72d3d7198b6f275949b53a
Tobias Neumann <gargamil@gmail.com>
2018-04-13 17:42:52 +0200

    fix for lint

--- app/webFrontend/src/app/user/user-edit/user-edit.component.ts
+++ app/webFrontend/src/app/user/user-edit/user-edit.component.ts
@@ -88,7 +88,7 @@ export class UserEditComponent implements OnInit {
   prepareSaveUser(): IUser {
     const userFormModel = this.userForm.value
 
-    if(this.user.profile.picture) {
+    if (this.user.profile.picture) {
       userFormModel.profile['picture'] = this.user.profile.picture;
     }
 
