cadf013433bfa046cec2d95b1848268ae9a40288
Chris <christopher.nierobisch@freenet.de>
2018-04-16 22:33:04 +0200

    Applied linting

--- app/webFrontend/src/app/models/User.ts
+++ app/webFrontend/src/app/models/User.ts
@@ -1,36 +1,37 @@
 import {IUser} from '../../../../../shared/models/IUser';
 import {IFile} from '../../../../../shared/models/IFile';
-const md5 = require("blueimp-md5");
+
+const md5 = require('blueimp-md5');
 
 export class User implements IUser {
-  _id: any;
-  uid: any;
-  email: string;
-  password: string;
-  profile: { firstName: string; lastName: string; picture: IFile; theme: string };
-  role: string;
-  progress: any;
-  lastVisitedCourses: Array<string>;
+    _id: any;
+    uid: any;
+    email: string;
+    password: string;
+    profile: { firstName: string; lastName: string; picture: IFile; theme: string };
+    role: string;
+    progress: any;
+    lastVisitedCourses: Array<string>;
 
-  constructor(user: IUser) {
-    this._id = user._id;
-    this.uid = user.uid;
-    this.email = user.email;
-    this.profile = user.profile;
-    this.role = user.role;
-    this.progress = user.progress;
-    this.lastVisitedCourses = user.lastVisitedCourses;
-  }
-
-  getGravatarURL(size: number = 80) {
-    return `https://www.gravatar.com/avatar/${md5(this.email.toLowerCase())}.jpg?s=${size}&d=retro`;
-  }
-
-  getUserImageURL(size: number = 80) {
-    if (this.profile && this.profile.picture) {
-      return 'api/uploads/users/' + this.profile.picture.name;
-    } else {
-      return this.getGravatarURL(size);
+    constructor(user: IUser) {
+        this._id = user._id;
+        this.uid = user.uid;
+        this.email = user.email;
+        this.profile = user.profile;
+        this.role = user.role;
+        this.progress = user.progress;
+        this.lastVisitedCourses = user.lastVisitedCourses;
     }
-  }
+
+    getGravatarURL(size: number = 80) {
+        return `https://www.gravatar.com/avatar/${md5(this.email.toLowerCase())}.jpg?s=${size}&d=retro`;
+    }
+
+    getUserImageURL(size: number = 80) {
+        if (this.profile && this.profile.picture) {
+            return 'api/uploads/users/' + this.profile.picture.name;
+        } else {
+            return this.getGravatarURL(size);
+        }
+    }
 }
