de04bf3e2c9257866b249c4ddaa87b0e5403c1f8
Florian <witulski.florian@gmail.com>
2018-04-21 18:41:34 +0200

    added user picture and profile information to dialog added styling updated link in course edit member list to open dialog

--- app/webFrontend/src/app/shared/components/user-profile-dialog/user-profile-dialog.component.ts
+++ app/webFrontend/src/app/shared/components/user-profile-dialog/user-profile-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {MatDialogRef} from '@angular/material';
 import {User} from "../../../models/User";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-user-profile-dialog',
@@ -11,7 +12,11 @@ export class UserProfileDialog {
 
   @Input() user: User;
 
-  constructor(public dialogRef: MatDialogRef<UserProfileDialog>) {
+  constructor(public dialogRef: MatDialogRef<UserProfileDialog>,
+              public userService: UserService) {
   }
 
+  canSeeUid() {
+    return (this.userService.isAdmin() || this.userService.isTeacher()) && this.user.uid;
+  }
 }
