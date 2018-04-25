0e71863f22af6fa83629317f0520da300ed31f36
PatrickSkowronek <patrick.skowronek@stud.h-da.de>
2018-04-25 20:35:51 +0200

    some more cleanup :)

--- .travis/changelog.sh
+++ .travis/changelog.sh
@@ -13,32 +13,32 @@ echo
 
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "develop" ]; then
- echo "+ detected pull request from ($TRAVIS_PULL_REQUEST_BRANCH) to $TRAVIS_BRANCH"
- curl https://raw.githubusercontent.com/h-da/geli/develop/CHANGELOG.md \
- | diff CHANGELOG.md - \
- | grep -P '^< - .{8,100}' - -q 
+  echo "+ detected pull request from ($TRAVIS_PULL_REQUEST_BRANCH) to $TRAVIS_BRANCH"
+  curl https://raw.githubusercontent.com/h-da/geli/develop/CHANGELOG.md \
+  | diff CHANGELOG.md - \
+  | grep -P '^< - .{8,100}' - -q 
 
- if [ $? ]; then
-  echo -e "${GREEN}+ Update in CHANGELOG.md found, exit${NC}"
+  if [ $? ]; then
+    echo -e "${GREEN}+ Update in CHANGELOG.md found, exit${NC}"
   else
-   echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found!"
-   echo -e "+ Please check if a line was added in the CHANGELOG.md.${NC}"
-   exit 1
+    echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found!"
+    echo -e "+ Please check if a line was added in the CHANGELOG.md.${NC}"
+    exit 1
   fi
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
- echo "+ detected pull request from ($TRAVIS_PULL_REQUEST_BRANCH) to $TRAVIS_BRANCH"
- curl https://raw.githubusercontent.com/h-da/geli/master/CHANGELOG.md \
- | diff CHANGELOG.md - \
- | grep -P '^< ## \[\d{1,3}\.\d{1,3}\.\d{1,3}\] - \d{4}-\d{2}-\d{2} - .{10,}' - -q 
+  echo "+ detected pull request from ($TRAVIS_PULL_REQUEST_BRANCH) to $TRAVIS_BRANCH"
+  curl https://raw.githubusercontent.com/h-da/geli/master/CHANGELOG.md \
+  | diff CHANGELOG.md - \
+  | grep -P '^< ## \[\d{1,3}\.\d{1,3}\.\d{1,3}\] - \d{4}-\d{2}-\d{2} - .{10,}' - -q 
 
- if [ $? ]; then
-  echo -e "${GREEN}+ Update in CHANGELOG.md found, exit${NC}"
+  if [ $? ]; then
+    echo -e "${GREEN}+ Update in CHANGELOG.md found, exit${NC}"
   else
-   echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found!"
-   echo -e "+ Please check if a new version was added in the CHANGELOG.md."
-   echo -e "+ Or a new section for the next release was added${NC}"
-   exit 1
+    echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found!"
+    echo -e "+ Please check if a new version was added in the CHANGELOG.md."
+    echo -e "+ Or a new section for the next release was added${NC}"
+    exit 1
   fi
 else
-  echo -e "${YELLOW}+ WARNING: No Pull Request agiainst Develop or Master -> skipping automate changelog checking${NC}";
+   echo -e "${YELLOW}+ WARNING: No Pull Request agiainst Develop or Master -> skipping automate changelog checking${NC}";
 fi
