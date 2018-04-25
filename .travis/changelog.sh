802cf7c50698f7860b93ae665b2c5a490aeb0b15
PatrickSkowronek <patrick.skowronek@stud.h-da.de>
2018-04-25 18:20:12 +0200

    More requested changes

--- .travis/changelog.sh
+++ .travis/changelog.sh
@@ -8,30 +8,35 @@ IPWD="$(pwd)"
 . ${DIR}/_shared-vars.sh
 
 echo
-echo "+++ Check if changelog was updadet +++"
+echo "+++ Check if changelog was updated +++"
 echo
 
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "develop" ]; then
-  curl https://raw.githubusercontent.com/h-da/geli/develop/CHANGELOG.md --output CHANGELOG.file
-  diff ../CHANGELOG.md CHANGELOG.file > diff.file
+ curl https://raw.githubusercontent.com/h-da/geli/develop/CHANGELOG.md |
+ diff CHANGELOG.md - | 
+ grep -P '^< - .{8,100}' - -q 
 
-  if grep -P '^< - .{8,100}' -q diff.file; then
-     echo "+ Update in CHANGELOG.md found"
+ if [ $? ]; then
+  echo "${GREEN}+ Update in CHANGELOG.md found, exit${NC}"
   else
-     echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found exit${NC}"
-     exit 1
+   echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found!"
+   echo -e "+ Please check if a line was added in the CHANGELOG.md.${NC}"
+   exit 1
   fi
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  curl https://raw.githubusercontent.com/h-da/geli/master/CHANGELOG.md --output CHANGELOG.file
-  diff ../CHANGELOG.md CHANGELOG.file > diff.file
+ curl https://raw.githubusercontent.com/h-da/geli/master/CHANGELOG.md |
+ diff CHANGELOG.md - | 
+ grep -P '^< ## \[\d\.\d\.\d\] - .{6,20}' - -q 
 
-  if grep -P '^< - .{8,100}' -q diff.file; then
-     echo "+ Update in CHANGELOG.md found"
+ if [ $? ]; then
+  echo "${GREEN}+ Update in CHANGELOG.md found, exit${NC}"
   else
-     echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found exit${NC}"
-     exit 1
+   echo -e "${RED}+ ERROR: No Update in CHANGELOG.md found!"
+   echo -e "+ Please check if a new version was added in the CHANGELOG.md."
+   echo -e "+ Or a new section for the next release was added${NC}"
+   exit 1
   fi
 else
-	echo -e "${YELLOW}+ WARNING: pull request #$TRAVIS_PULL_REQUEST -> skipping automate changelog checking${NC}";
+  echo -e "${YELLOW}+ WARNING: No Pull Request agiainst Develop or Master -> skipping automate changelog checking${NC}";
 fi
