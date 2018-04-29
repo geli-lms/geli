ca0cf0fd5bd7a87a29bc253239e300801e8ce1cf
HPunktOchs <henrik.ochs@arcor.de>
2018-04-29 17:34:42 +0200

    seems to work with a romise, somehow we lost functionality. quicksaving..

--- app/webFrontend/src/app/course/course-detail/course-detail.component.ts
+++ app/webFrontend/src/app/course/course-detail/course-detail.component.ts
@@ -35,9 +35,8 @@ export class CourseDetailComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-
-    // this.route.data.route.subscribe
-    //const course: ICourse = <ICourse> this.route.snapshot.data.course;
+    const data: any = this.route.snapshot.data;
+    const course: ICourse = <ICourse> data.course.then;
     console.dir(course);
     if (course) {
       console.log('found course');
--- app/webFrontend/src/app/shared/services/course-detail-data-resolve.service.ts
+++ app/webFrontend/src/app/shared/services/course-detail-data-resolve.service.ts
@@ -1,16 +1,14 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot, Resolve, ActivatedRoute} from '@angular/router';
-import {AuthenticationService} from './authentication.service';
 import {UserService} from './user.service';
 import {ICourse} from '../../../../../../shared/models/ICourse';
-import {LastVisitedCourseContainerUpdater} from '../utils/LastVisitedCourseContainerUpdater';
 import {CourseService, UserDataService} from './data.service';
 import {MatSnackBar} from '@angular/material';
 import {DataSharingService} from './data-sharing.service';
 import {Observable} from 'rxjs/Rx';
 
 @Injectable()
-export class CourseDetailDataResolve implements Resolve<ICourse> {
+export class CourseDetailDataResolve implements Resolve<Promise<ICourse>> {
 
   constructor(private courseService: CourseService,
               private router: Router,
@@ -20,21 +18,8 @@ export class CourseDetailDataResolve implements Resolve<ICourse> {
               private shareDataService: DataSharingService) {
   }
 
-  resolve(route: ActivatedRouteSnapshot): Observable<ICourse> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<ICourse> {
     const id = route.paramMap.get('id');
-    this.courseService.readSingleItem(id).then(
-      (course: any) => {
-        return Observable.of(course);
-      },(errorResponse: Response) => {
-        if (errorResponse.status === 401) {
-          this.snackBar.open('You are not authorized to view this course.', '', {duration: 3000});
-        }
-        if (errorResponse.status === 404) {
-          this.snackBar.open('Your selected course is not available.', '', {duration: 3000});
-          this.router.navigate(['/not-found']);
-
-        }
-      });
-    return Observable.empty();
+    return this.courseService.readSingleItem(id);
   }
 }
