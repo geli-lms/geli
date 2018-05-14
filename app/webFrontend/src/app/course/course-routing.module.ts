3b2187fd130c97394a42623a724c0ed533fdeb6b
torss <37229901+torss@users.noreply.github.com>
2018-05-14 18:50:07 +0200

    App: Use shared roles.ts

--- app/webFrontend/src/app/course/course-routing.module.ts
+++ app/webFrontend/src/app/course/course-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuardService} from '../shared/services/auth-guard.service';
 import {CourseNewComponent} from './course-new/course-new.component';
 import {CourseDetailComponent} from './course-detail/course-detail.component';
+import {allRoles} from '../../../../../shared/roles';
 
 const routes: Routes = [
   {
@@ -15,7 +16,7 @@ const routes: Routes = [
     path: ':id',
     component: CourseDetailComponent,
     canActivate: [AuthGuardService],
-    data: {roles: ['student', 'teacher', 'admin']} // Currently unused / disabled: 'tutor',
+    data: {roles: allRoles}
   },
   {
     path: ':id/edit',
@@ -29,13 +30,13 @@ const routes: Routes = [
     path: ':id/lecture/:lecture',
     component: CourseDetailComponent,
     canActivate: [AuthGuardService],
-    data: {roles: ['student', 'teacher', 'admin']} // Currently unused / disabled: 'tutor',
+    data: {roles: allRoles}
   },
   {
     path: ':id/unit/:unit',
     component: CourseDetailComponent,
     canActivate: [AuthGuardService],
-    data: {roles: ['student', 'teacher', 'admin']} // Currently unused / disabled: 'tutor',
+    data: {roles: allRoles}
   }
 ];
 
