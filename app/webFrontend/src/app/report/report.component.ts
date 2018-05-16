14e8b63f4cd6581fbeea4466276af86f0e29443a
MichaelNarkus <michael.narkus@stud.h-da.de>
2018-05-16 23:38:41 +0200

    Prepared Content of Report for Csv Creation

--- app/webFrontend/src/app/report/report.component.ts
+++ app/webFrontend/src/app/report/report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {UserService} from '../shared/services/user.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CourseService} from '../shared/services/data.service';
+import {ReportService} from "../shared/services/data/report.service";
 
 @Component({
   selector: 'app-report',
@@ -18,7 +19,8 @@ export class ReportComponent implements OnInit {
     public userService: UserService,
     private route: ActivatedRoute,
     private router: Router,
-    private courseService: CourseService) { }
+    private courseService: CourseService,
+    private reportService: ReportService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -34,5 +36,58 @@ export class ReportComponent implements OnInit {
       })
       .catch((err) => {
       });
+  }
+
+  private exportReport() {
+    this.reportService.getCourseResults(this.courseId)
+      .then((result) => {
+        let report = result;
+        this.generateCsvContent(report);
+    });
+  }
+
+  private generateCsvContent(report: any[]) {
+    let reportData: any[] = [];
+    for (let student of report) {
+      for (let unit of student.progress.units) {
+        let status: string;
+        let answers: string;
+        if (unit.__t === "task") {
+          if (unit.done) {
+            status = "done;";
+          }
+          else {
+            status = "tried;";
+          }
+
+          for (let answer of unit.tasks[0].answers) {
+            if (answer.value) {
+              answers += answer.text + ";";
+            }
+          }
+
+        }
+        else if (unit.__t === "code-kata") {
+          if (unit.progressData.done) {
+            status = "done";
+          }
+          else {
+            status = "tried"
+          }
+        }
+
+        let entry = {
+          id : student.uid,
+          lastName: student.profile.lastName,
+          firstName: student.profile.firstName,
+          unit: unit.name,
+          unitType: unit.__t,
+          status: status,
+          answers: null,
+        };
+        reportData.push(entry);
+      }
+    }
+    console.log(reportData);
   }
 }
