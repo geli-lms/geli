d4902d47a9a5651a99183bd6b79fcbfd1eb13589
AlperUygun <38135352+AlperUygun@users.noreply.github.com>
2018-05-21 15:26:22 +0200

    Update dashboard-admin.component.ts

--- app/webFrontend/src/app/start/dashboard/dashboard-admin/dashboard-admin.component.ts
+++ app/webFrontend/src/app/start/dashboard/dashboard-admin/dashboard-admin.component.ts
@@ -3,6 +3,8 @@ import {DashboardBaseComponent} from '../dashboard-base-component';
 import {SnackBarService} from '../../../shared/services/snack-bar.service';
 import {DialogService} from '../../../shared/services/dialog.service';
 import {Router} from '@angular/router';
+import {SortUtil} from "../../../shared/utils/SortUtil";
+import {ICourseDashboard} from "../../../../../../../shared/models/ICourseDashboard";
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -12,6 +14,8 @@ import {Router} from '@angular/router';
 export class DashboardAdminComponent extends DashboardBaseComponent {
 
   fabOpen = false;
+  searchValue: string;
+  allCoursesAdmin: ICourseDashboard[];
 
   constructor(private snackBar: SnackBarService,
               private router: Router,
@@ -20,6 +24,22 @@ export class DashboardAdminComponent extends DashboardBaseComponent {
   }
 
   ngOnInit() {
+    this.searchValue = '';
+    this.allCoursesAdmin = [];
+  }
+
+  async filterCourses(event: any) {
+    this.searchValue = event.target.value.toLowerCase();
+    if (this.allCoursesAdmin.length == 0) {
+      this.allCoursesAdmin = this.allCourses;
+    }
+    this.allCourses = [];
+    for (const course of this.allCoursesAdmin) {
+      const temp = course.name.toLowerCase();
+      if (temp.includes(this.searchValue)) {
+        this.allCourses.push(course);
+      }
+    }
   }
 
   closeFab = () => {
