5473f103d5656e98df61d5cfdad880c1a28dd82c
David Boschmann <DavidBoschmann@live.de>
2018-06-13 15:13:10 +0200

    Created Component Emoji Selector

--- app/webFrontend/src/app/forum/forum.module.ts
+++ app/webFrontend/src/app/forum/forum.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmojiSelectorComponent } from './emoji-selector/emoji-selector.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [EmojiSelectorComponent]
 })
 export class ForumModule { }
