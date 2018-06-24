a4d0f35bcebe30d899153a8380eb3da6d4ff6f6b
PatrickSkowronek <patrick.skowronek@stud.h-da.de>
2018-06-24 12:14:20 +0200

    Moved Cookiebanner to the right place

--- app/webFrontend/src/app/privacy/privacy.module.ts
+++ app/webFrontend/src/app/privacy/privacy.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import {LegalnoticeComponent} from './legalnotice/legalnotice.component';
 import {PrivacyComponent} from './privacynotice/privacy.component';
+import {CookieBannerComponent} from './cookie-banner/cookie-banner.component';
 import {TitleService} from '../shared/services/title.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   declarations: [
     LegalnoticeComponent,
-    PrivacyComponent
+    PrivacyComponent,
+    CookieBannerComponent
+  ],
+  exports: [
+    CookieBannerComponent
   ]
 })
 export class PrivacyModule { }
