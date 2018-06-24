5d6250d6f0d867d33f5923dc2c4d201ac9c0997c
Max <max.leppla@gmx.de>
2018-06-24 23:08:12 +0200

    - viewport changes
    - privacy.module -> remove unused imports

--- app/webFrontend/src/app/privacy/privacy.module.ts
+++ app/webFrontend/src/app/privacy/privacy.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
 import {LegalnoticeComponent} from './legalnotice/legalnotice.component';
 import {PrivacyComponent} from './privacynotice/privacy.component';
 import {CookieBannerComponent} from './cookie-banner/cookie-banner.component';
-import {TitleService} from '../shared/services/title.service';
-import {MaterialImportModule} from '../shared/modules/material-import.module';
 import {MatButtonModule} from '@angular/material';
 
 @NgModule({
