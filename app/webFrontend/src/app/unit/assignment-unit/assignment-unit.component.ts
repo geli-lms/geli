e1105010b4b97dfcbb0ff7df2a606bae3ab022f2
Chris <christopher.nierobisch@freenet.de>
2018-06-26 09:51:29 +0200

    #600: Add assignments Added uploadform to the student view

--- app/webFrontend/src/app/unit/assignment-unit/assignment-unit.component.ts
+++ app/webFrontend/src/app/unit/assignment-unit/assignment-unit.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {UnitFormService} from "../../shared/services/unit-form.service";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {IAssignmentUnit} from "../../../../../../shared/models/units/IAssignmentUnit";
+import {UploadFormComponent} from "../../shared/components/upload-form/upload-form.component";
+import {IFileUnit} from "../../../../../../shared/models/units/IFileUnit";
 
 @Component({
   selector: 'app-assignment-unit',
@@ -9,26 +11,53 @@ import {IAssignmentUnit} from "../../../../../../shared/models/units/IAssignment
   styleUrls: ['./assignment-unit.component.scss']
 })
 export class AssignmentUnitComponent implements OnInit {
+    @Input() assignmentUnit: IAssignmentUnit;
+    @ViewChild(UploadFormComponent) public uploadForm: UploadFormComponent;
+
+    allowedMimeTypes: string[];
     unitForm: FormGroup;
-    model: IAssignmentUnit;
+    uploadPath: string;
 
     constructor(public unitFormService: UnitFormService,
                 public formBuilder: FormBuilder){ }
 
     ngOnInit() {
+        this.uploadPath = '/api/units/' + this.assignmentUnit._id + '/assignment';
+        this.allowedMimeTypes = ['text/plain'];
+
         this.unitFormService.unitInfoText = 'Assignments Info';
         this.unitFormService.headline = 'Assignments';
 
         this.unitForm = this.unitFormService.unitForm;
 
-        this.unitForm.addControl('deadline', new FormControl(this.model.deadline));
-        this.unitForm.addControl('assignments', new FormControl(this.model.assignments));
+        this.unitForm.addControl('deadline', new FormControl(this.assignmentUnit.deadline));
+        this.unitForm.addControl('assignments', new FormControl(this.assignmentUnit.assignments));
 
-        if (this.model) {
+        if (this.assignmentUnit) {
             this.unitForm.patchValue({
-                deadline: this.model.deadline,
-                assignments: this.model.assignments
+                deadline: this.assignmentUnit.deadline,
+                assignments: this.assignmentUnit.assignments
             });
+        }
+    }
+
+    public startUpload() {
+        try {
+            this.uploadForm.fileUploader.uploadAll();
+        } catch (error) {
+        }
+    }
+
+    public onFileUploaded(event: IFileUnit) {
+    }
+
+    public onAllUploaded() {
+        this.dialogRef.close(true);
+    }
+
+    public isObjectInQueue() {
+        if (this.uploadForm) {
+            return this.uploadForm.fileUploader.queue.length > 0;
         }
     }
 }
