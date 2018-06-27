6a37c7f0bf9cbd5d4fb63aff9c6d3e82ca63a5fe
PatrickSkowronek <patrick.skowronek@stud.h-da.de>
2018-06-27 21:31:10 +0200

    Fixed linting error

--- app/webFrontend/src/app/privacy/cookie-banner/cookie-banner.component.ts
+++ app/webFrontend/src/app/privacy/cookie-banner/cookie-banner.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./cookie-banner.component.scss']
 })
 export class CookieBannerComponent implements OnInit {
-  
+
   isCookieAccepted: boolean;
 
   constructor() {
