45e734ec74c5d93909bff19ec4cd3a54d429afe3
torss <37229901+torss@users.noreply.github.com>
2018-07-18 18:55:03 +0200

    App: Add new AdminMiscComponent

---
+++ app/webFrontend/src/app/admin/admin-misc/admin-misc.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-misc',
+  templateUrl: './admin-misc.component.html',
+  styleUrls: ['./admin-misc.component.scss']
+})
+export class AdminMiscComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
