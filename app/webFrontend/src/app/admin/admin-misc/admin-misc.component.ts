74728cfb2458f75229be741ef04bfd4ae6366369
torss <37229901+torss@users.noreply.github.com>
2018-07-18 20:04:08 +0200

    App: Admin clear cache button (in Misc. area)

--- app/webFrontend/src/app/admin/admin-misc/admin-misc.component.ts
+++ app/webFrontend/src/app/admin/admin-misc/admin-misc.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {BackendService} from '../../shared/services/backend.service';
+import {SnackBarService} from '../../shared/services/snack-bar.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin-misc',
@@ -7,9 +10,21 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdminMiscComponent implements OnInit {
 
-  constructor() { }
+  constructor(private backendService: BackendService,
+              private snackBar: SnackBarService,
+              private translate: TranslateService) {
+  }
 
   ngOnInit() {
+  }
+
+  async clearCache() {
+    try {
+      await this.backendService.delete('download/cache').toPromise();
+    } catch (error) {
+      const msgPrefix = await this.translate.get('admin.cache.clear.failed').toPromise();
+      this.snackBar.openLong(msgPrefix + ': ' + error.message);
+    }
   }
 
 }
