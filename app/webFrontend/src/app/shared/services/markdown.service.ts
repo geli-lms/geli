6f86aa6afddbadf4e50c35d3a5857f27dfa13ac7
Armel Siewe <Brice.A.Siewe@stud.h-da.de>
2018-08-05 20:26:16 +0200

    Forum: fix error "parse was not found in twemoji"

--- app/webFrontend/src/app/shared/services/markdown.service.ts
+++ app/webFrontend/src/app/shared/services/markdown.service.ts
@@ -1,27 +1,22 @@
 import {Injectable} from '@angular/core';
 import * as MarkdownIt from 'markdown-it';
 import * as markdownItEmoji from 'markdown-it-emoji';
-import * as twemoji from 'twemoji';
-
-const emojis = require('../../../../node_modules/markdown-it-emoji/lib/data/full.json');
-
+import twemoji from 'twemoji'
 
 @Injectable()
 export class MarkdownService {
   markdown: any;
-  emojis: any[];
 
 
   constructor() {
     this.markdown = new MarkdownIt();
     this.markdown.use(markdownItEmoji);
-    this.emojis = emojis;
     this.markdown.renderer.rules.emoji = function (token, idx) {
       return twemoji.parse(token[idx].content);
     };
   }
 
   render(text: string): string {
-   return  this.markdown.render(text);
+    return this.markdown.render(text);
   }
 }
