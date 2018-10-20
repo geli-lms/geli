82679005547416f623b4d7b5a66eede4b1021d81
Daniel Kesselberg <mail@danielkesselberg.de>
2018-10-20 14:25:52 +0200

    Fix depcreated warning

--- api/src/migrations/MigrationHandler.ts
+++ api/src/migrations/MigrationHandler.ts
@@ -13,7 +13,7 @@ export class MigrationHandler {
     (<any>mongoose).Promise = global.Promise;
 
     if (!mongoose.connection.readyState) {
-      this.databaseConnection = mongoose.connect(config.database);
+      this.databaseConnection = mongoose.connect(config.database, {useNewUrlParser: true});
     }
 
     fs.readdirSync(__dirname + '/scripts').forEach((file: string) => {
