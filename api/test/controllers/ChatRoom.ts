638524bd697d68b006e7ae678c799bfe38eb380c
Daniel Kesselberg <mail@danielkesselberg.de>
2018-10-20 15:18:56 +0200

    Use new migration name

--- api/test/controllers/ChatRoom.ts
+++ api/test/controllers/ChatRoom.ts
@@ -26,7 +26,7 @@ describe('ChatRoom', async () => {
     // load fixtures
     await fixtureLoader.load();
     // run migrations for chat feature
-    await (new MigrationHandler()).up(['course_v2', 'unit_v2']);
+    await (new MigrationHandler()).up(['20180821-course', '20180821-unit']);
   });
 
   describe(`GET ${BASE_URL} :id`, async () => {
