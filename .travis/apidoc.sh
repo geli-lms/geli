c3e9b0090f40e975f19f5a39f827895e3c29e293
Daniel Kesselberg <mail@danielkesselberg.de>
2018-10-21 17:43:26 +0200

    Merge develop / master block

--- .travis/apidoc.sh
+++ .travis/apidoc.sh
@@ -29,44 +29,30 @@ mkdir -p ${GITHUB_DIR}
 eval "$(ssh-agent -s)"
 ssh-add <(echo "${GIT_PRIVATE_KEY}")
 
-git config --global user.name "Travis"
-git config --global user.email "travis@travis-ci.org"
-
-if [[ ${TRAVIS_BRANCH} == "master" ]] || [[ -n ${TRAVIS_TAG} ]]; then
+if [[ ${TRAVIS_BRANCH} == "master" ]] || [[ ${TRAVIS_BRANCH} == "develop" ]] || [[ -n ${TRAVIS_TAG} ]]; then
   echo "+ Generate API-Doc"
   cd api || exit 1
   npm run apidoc
 
   echo "+ Publish API-Doc to ${GITHUB_URL}"
-  git clone ${GITHUB_URL} -b master ${GITHUB_DIR}
+  if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
+    git clone ${GITHUB_URL} -b develop ${GITHUB_DIR}
+  else
+    git clone ${GITHUB_URL} -b master ${GITHUB_DIR}
+  fi
 
   rsync -a -delete apidocs/ ${GITHUB_DIR}
-
   cd ${GITHUB_DIR} || exit 1
+
+  git config user.name "Travis"
+  git config user.email "travis@travis-ci.org"
+
   git add --all
   git commit -m "Travis build: ${TRAVIS_BUILD_NUMBER}"
 
   if [[ -n ${TRAVIS_TAG} ]]; then
     git tag -a "${TRAVIS_TAG}" -m "Release ${TRAVIS_TAG}"
   fi
-
-  git push --follow-tags ${GITHUB_URL}
-  exit 0
-fi
-
-if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
-  echo "+ Generate API-Doc"
-  cd api || exit 1
-  npm run apidoc
-
-  echo "+ Publish API-Doc to ${GITHUB_URL}"
-  git clone ${GITHUB_URL} -b develop ${GITHUB_DIR}
-
-  rsync -a -delete apidocs/ ${GITHUB_DIR}
-
-  cd ${GITHUB_DIR} || exit 1
-  git add --all
-  git commit -m "Travis build: ${TRAVIS_BUILD_NUMBER}"
 
   git push --follow-tags ${GITHUB_URL}
   exit 0
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -59,7 +59,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Update `Node.js` version to `10.8.0`. [#821](https://github.com/geli-lms/geli/pull/821)
 - Update `README.md` with latest information. [#845](https://github.com/geli-lms/geli/pull/845)
 - Exit build when no change to `CHANGELOG.md`. [#880](https://github.com/geli-lms/geli/pull/880)
-- Use deploy token to push geli-docs [#851](https://github.com/geli-lms/geli/issues/851)
+- Use deploy token to push geli-docs. [#851](https://github.com/geli-lms/geli/issues/851)
 
 ### Removed
 - `isCourseTeacherOrAdmin` and `isMemberOfCourse` from `UserService`. [#731](https://github.com/geli-lms/geli/issues/731)
