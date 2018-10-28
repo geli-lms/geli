2cf280fe7448b4cee6c89e2ef2ffe496e02a645c
MichaelNarkus <michael.narkus@stud.h-da.de>
2018-10-28 16:53:52 +0100

    Added CodeKataValidationService

---
+++ app/webFrontend/src/app/shared/services/code-kata-validation.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CodeKataValidationService {
+
+  constructor() { }
+
+  validate(codeToTest: string) {
+    let result = false;
+    let logs = '';
+    const origLogger = window.console.log;
+    window.console.log = (msg) => {
+      logs += msg + '\n';
+    };
+    try {
+      // tslint:disable-next-line:no-eval
+      result = eval(codeToTest);
+    } catch (e) {
+      const err = e.constructor('Error in evaled Script: ' + e.message);
+      err.lineNumber = e.lineNumber - err.lineNumber;
+      const msg = 'Error ' + e.message;
+      // tslint:disable-next-line:no-console
+      console.log(msg);
+      // tslint:disable-next-line:no-console
+      console.error(err);
+    }
+    return { result: result, log: logs};
+  }
+}
