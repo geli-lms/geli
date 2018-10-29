758b30fe98c698e6874c77bf03a596211bc04855
Daniel Kesselberg <mail@danielkesselberg.de>
2018-10-29 20:59:29 +0100

    fixup! Add text to i18n

--- app/webFrontend/src/app/course/course-detail/course-detail.component.ts
+++ app/webFrontend/src/app/course/course-detail/course-detail.component.ts
@@ -56,7 +56,7 @@ export class CourseDetailComponent implements OnInit, OnDestroy {
         this.tabs = [{path: 'overview', label: t['common.content'], img: 'school'},
           {path: 'fileview', label: t['common.documents'], img: 'insert_drive_file'},
           {path: 'videoview', label: t['common.videos'], img: 'video_library'},
-          {path: 'download', label: t['common.download'] + ' (Beta)', img: 'get_app'}];
+          {path: 'download', label: t['common.download'], img: 'get_app'}];
       });
   }
 
