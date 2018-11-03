b1364e0cd92fe6b1dc6763eed86c871cd8b35b1f
Tobias Neumann <gargamil@gmail.com>
2018-11-03 13:29:28 +0100

    removed anchors in footnotes

--- app/webFrontend/src/app/shared/services/markdown.service.ts
+++ app/webFrontend/src/app/shared/services/markdown.service.ts
@@ -20,10 +20,12 @@ export class MarkdownService {
     this.markdown.use(markdownItEmoji);
     this.markdown.use(MarkdownItDeflist);
     this.markdown.use(MarkdownItFootnote);
+
     this.markdown.use(MarkdownItContainer);
     this.markdown.use(MarkdownItMark);
     this.markdown.use(MarkdownItAbbr);
 
+    this.overwriteCustomFootnoteRenderer();
     this.markdown.renderer.rules.emoji = function (token, idx) {
       return twemoji.parse(token[idx].content);
     };
@@ -31,5 +33,30 @@ export class MarkdownService {
 
   render(text: string): string {
     return this.markdown.render(text);
+  }
+
+  overwriteCustomFootnoteRenderer() {
+    this.markdown.renderer.rules.footnote_ref = function (tokens, idx) {
+      var n = Number(tokens[idx].meta.id + 1).toString();
+      var id = 'fnref' + n;
+      var uri = window.location.pathname;
+      if (tokens[idx].meta.subId > 0) {
+        id += ':' + tokens[idx].meta.subId;
+      }
+      //return '<sup class="footnote-ref"><a href="' + uri + '#fn' + n + '" id="' + id + '">[' + n + ']</a></sup>';
+      return '<sup class="footnote-ref">[' + n + ']</sup>';
+    };
+
+    this.markdown.renderer.rules.footnote_anchor = function (tokens, idx) {
+      var n = Number(tokens[idx].meta.id + 1).toString();
+      var id = 'fnref' + n;
+      var uri = window.location.pathname;
+      if (tokens[idx].meta.subId > 0) {
+        id += ':' + tokens[idx].meta.subId;
+      }
+      //return ' <a href="' + uri + '#' + id + '" class="footnote-backref">\u21a9</a>';
+      return '<span class="footnote-backref></span>'
+      /* ↩ */
+    };
   }
 }
