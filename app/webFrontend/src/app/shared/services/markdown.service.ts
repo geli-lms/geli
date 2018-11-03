ff329736ff73598d5efea8a1bba0ad9df193866b
Tobias Neumann <gargamil@gmail.com>
2018-11-03 13:00:33 +0100

    added markdown-it plugins

--- app/webFrontend/src/app/shared/services/markdown.service.ts
+++ app/webFrontend/src/app/shared/services/markdown.service.ts
@@ -2,6 +2,11 @@ import {Injectable} from '@angular/core';
 import * as MarkdownIt from 'markdown-it';
 import * as markdownItEmoji from 'markdown-it-emoji';
 import twemoji from 'twemoji';
+import * as MarkdownItDeflist from 'markdown-it-deflist';
+import * as MarkdownItFootnote from 'markdown-it-footnote';
+import * as MarkdownItContainer from 'markdown-it-container';
+import * as MarkdownItMark from 'markdown-it-mark';
+import * as MarkdownItAbbr from 'markdown-it-abbr';
 
 @Injectable()
 export class MarkdownService {
@@ -10,7 +15,15 @@ export class MarkdownService {
 
   constructor() {
     this.markdown = new MarkdownIt();
+
+    // load plugins
     this.markdown.use(markdownItEmoji);
+    this.markdown.use(MarkdownItDeflist);
+    this.markdown.use(MarkdownItFootnote);
+    this.markdown.use(MarkdownItContainer);
+    this.markdown.use(MarkdownItMark);
+    this.markdown.use(MarkdownItAbbr);
+
     this.markdown.renderer.rules.emoji = function (token, idx) {
       return twemoji.parse(token[idx].content);
     };
