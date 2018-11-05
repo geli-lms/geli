4ef7904960e152aa30f4e31aba0819557bc98b58
MichaelNarkus <michael.narkus@stud.h-da.de>
2018-11-05 15:24:03 +0100

    Added console.logs and errors to log in service

--- app/webFrontend/src/app/shared/services/code-kata-validation.service.ts
+++ app/webFrontend/src/app/shared/services/code-kata-validation.service.ts
@@ -7,15 +7,26 @@ export class CodeKataValidationService {
 
   validate(codeToTest: string) {
     let result = false;
-    let log = '';
+    let log = undefined;
+
+    window.console.log = (msg) => {
+      if (log === undefined) {
+        log = '';
+      }
+      log += msg + '\n';
+      // tslint:disable-next-line:no-console
+      console.log(msg);
+    };
 
     try {
+      // tslint:disable-next-line:no-eval
       result = eval(codeToTest);
-    }
-    catch (e) {
+    } catch (e) {
       const err = e.constructor('Error in evaled Script:' + e.message);
       err.lineNumber = e.lineNumber - err.lineNumber;
       log = log + err + '\n';
+      // tslint:disable-next-line:no-console
+      console.error(err.message);
     }
     return {result, log};
   }
