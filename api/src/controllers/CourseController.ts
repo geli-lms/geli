c8491982fda108a7da74cfe56b44976526ac7bf1
torss <37229901+torss@users.noreply.github.com>
2018-11-06 15:39:04 +0100

    API: Remove unused CourseController variable

--- api/src/controllers/CourseController.ts
+++ api/src/controllers/CourseController.ts
@@ -763,7 +763,7 @@ export class CourseController {
 
     await image.save();
 
-    const result = await Course.update({ _id: id }, {
+    await Course.update({ _id: id }, {
       image: image._id
     });
 
