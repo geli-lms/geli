8dcaec2dc5a9db822c15833f38f3614d23aef70d
torss <37229901+torss@users.noreply.github.com>
2018-11-08 13:31:25 +0100

    Improve SocketIOMessage interface sharing

--- api/src/models/SocketIOMessage.ts
+++ api/src/models/SocketIOMessage.ts
@@ -1,37 +1,4 @@
-export interface IMessage {
-  _id: any;
-  content: string;
-  visible?: boolean;
-  room: string;
-  chatName: string;
-  comments: IMessage[];
-  author: any;
-  updatedAt?: string;
-  createdAt?: string;
-}
-
-
-enum SocketIOMessageType {
+export enum SocketIOMessageType {
   COMMENT = 'comment',
   MESSAGE = 'message'
 }
-
-interface ISocketIOMessageMeta {
-  type: SocketIOMessageType;
-  parent: string;
-}
-
-
-// This is what clients receive.
-interface ISocketIOMessage {
-  meta: ISocketIOMessageMeta;
-  message: IMessage;
-}
-
-// This is what's "posted" by the client to the ChatServer.
-interface ISocketIOMessagePost {
-  meta: ISocketIOMessageMeta;
-  content: string;
-}
-
-export {ISocketIOMessagePost, ISocketIOMessage, ISocketIOMessageMeta, SocketIOMessageType};
