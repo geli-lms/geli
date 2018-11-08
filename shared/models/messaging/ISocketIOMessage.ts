dc0031bf5ed0c0a47e0333ad7ec8cae97f130613
torss <37229901+torss@users.noreply.github.com>
2018-11-08 15:21:25 +0100

    Shared: Refactor ISocketIOMessage exports

--- shared/models/messaging/ISocketIOMessage.ts
+++ shared/models/messaging/ISocketIOMessage.ts
@@ -1,25 +1,23 @@
 import {IMessage} from './IMessage';
 
-enum SocketIOMessageType {
+export enum SocketIOMessageType {
   COMMENT = 'comment',
   MESSAGE = 'message'
 }
 
-interface ISocketIOMessageMeta {
+export interface ISocketIOMessageMeta {
   type: SocketIOMessageType;
   parent: string;
 }
 
 // This is what clients receive.
-interface ISocketIOMessage {
+export interface ISocketIOMessage {
   meta: ISocketIOMessageMeta;
   message: IMessage;
 }
 
 // This is what's "posted" by the client to the ChatServer.
-interface ISocketIOMessagePost {
+export interface ISocketIOMessagePost {
   meta: ISocketIOMessageMeta;
   content: string;
 }
-
-export {ISocketIOMessagePost, ISocketIOMessage, ISocketIOMessageMeta, SocketIOMessageType};
