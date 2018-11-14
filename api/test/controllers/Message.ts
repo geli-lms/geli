a324bcaa5ff2282e0da84c4ebdb1ddeff0d99f6e
torss <37229901+torss@users.noreply.github.com>
2018-11-14 17:57:05 +0100

    API: Move shared /api/message test functions
    ...above the outer "describe" for (hopefully) better readability.

--- api/test/controllers/Message.ts
+++ api/test/controllers/Message.ts
@@ -21,55 +21,55 @@ function getRandomRoomFromCourse(course: ICourse): IChatRoom {
   return course.chatRooms[Math.floor(Math.random() * course.chatRooms.length)];
 }
 
+async function simpleRoomSetup() {
+  const course = await FixtureUtils.getRandomCourse() as ICourseModel;
+  const room = getRandomRoomFromCourse(course);
+  const roomId = room._id.toString();
+  return {course, room, roomId};
+}
+
+async function commonRequest(user: IUser, urlPostfix = '', queryOptions?: string | object) {
+  return await chai.request(app)
+    .get(BASE_URL + urlPostfix)
+    .query(queryOptions)
+    .set('Cookie', `token=${JwtUtils.generateToken(user)}`)
+    .catch((err) => err.response);
+}
+
+async function testMissingRoom(urlPostfix = '') {
+  const admin = await FixtureUtils.getRandomAdmin();
+
+  const result = await commonRequest(admin, urlPostfix);
+
+  expect(result).to.have.status(400);
+}
+
+async function testSuccess(urlPostfix = '') {
+  const {roomId} = await simpleRoomSetup();
+  const admin = await FixtureUtils.getRandomAdmin();
+
+  const result = await commonRequest(admin, urlPostfix, {room: roomId});
+
+  expect(result).to.have.status(200);
+  expect(result).to.be.json;
+  return result;
+}
+
+async function testAccessDenial(urlPostfix = '') {
+  const {course, roomId} = await simpleRoomSetup();
+  const student = await User.findOne({role: 'student', _id: {$nin: course.students}});
+
+  const result = await commonRequest(student, urlPostfix, {room: roomId});
+
+  expect(result).to.have.status(403);
+}
+
 describe('Message', async () => {
   // Before each test we reset the database
   beforeEach(async () => {
     // load fixtures
     await fixtureLoader.load();
   });
-
-  async function simpleRoomSetup() {
-    const course = await FixtureUtils.getRandomCourse() as ICourseModel;
-    const room = getRandomRoomFromCourse(course);
-    const roomId = room._id.toString();
-    return {course, room, roomId};
-  }
-
-  async function commonRequest(user: IUser, urlPostfix = '', queryOptions?: string | object) {
-    return await chai.request(app)
-      .get(BASE_URL + urlPostfix)
-      .query(queryOptions)
-      .set('Cookie', `token=${JwtUtils.generateToken(user)}`)
-      .catch((err) => err.response);
-  }
-
-  async function testMissingRoom(urlPostfix = '') {
-    const admin = await FixtureUtils.getRandomAdmin();
-
-    const result = await commonRequest(admin, urlPostfix);
-
-    expect(result).to.have.status(400);
-  }
-
-  async function testSuccess(urlPostfix = '') {
-    const {roomId} = await simpleRoomSetup();
-    const admin = await FixtureUtils.getRandomAdmin();
-
-    const result = await commonRequest(admin, urlPostfix, {room: roomId});
-
-    expect(result).to.have.status(200);
-    expect(result).to.be.json;
-    return result;
-  }
-
-  async function testAccessDenial(urlPostfix = '') {
-    const {course, roomId} = await simpleRoomSetup();
-    const student = await User.findOne({role: 'student', _id: {$nin: course.students}});
-
-    const result = await commonRequest(student, urlPostfix, {room: roomId});
-
-    expect(result).to.have.status(403);
-  }
 
   describe(`GET ${BASE_URL}`, async () => {
     it('should fail when parameter room missing', async () => {
