bd0ba5056d7d6b4e21bd3be7e66538842077c8ca
Daniel Kesselberg <mail@danielkesselberg.de>
2018-11-22 11:56:11 +0100

    Update => Save

--- api/test/controllers/TestNotificationController.ts
+++ api/test/controllers/TestNotificationController.ts
@@ -6,7 +6,7 @@ import {JwtUtils} from '../../src/security/JwtUtils';
 import {Notification} from '../../src/models/Notification';
 import chaiHttp = require('chai-http');
 import {User} from '../../src/models/User';
-import {Course} from '../../src/models/Course';
+import {Course, ICourseModel} from '../../src/models/Course';
 import {NotFoundError} from 'routing-controllers';
 import {API_NOTIFICATION_TYPE_ALL_CHANGES, API_NOTIFICATION_TYPE_NONE, NotificationSettings} from '../../src/models/NotificationSettings';
 
@@ -248,10 +248,11 @@ describe('Notifications', async () => {
 
   describe(`DELETE ${BASE_URL} :id`, () => {
     it('should delete a notification', async () => {
-      const course = await FixtureUtils.getRandomCourse();
+      const course = await <ICourseModel>FixtureUtils.getRandomCourse();
       const students = await FixtureUtils.getRandomStudents(2, 5);
+
       course.students = course.students.concat(students);
-      await Course.update(course, {new: true});
+      await course.save();
 
       const newNotification = await new Notification({
         user: students[0],
