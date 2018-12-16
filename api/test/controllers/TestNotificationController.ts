5f42b8c56492995f265b10b34d4a8f2b553d3f7b
torss <37229901+torss@users.noreply.github.com>
2018-12-16 19:47:06 +0100

    API: Replace .count with .countDocuments

--- api/test/controllers/TestNotificationController.ts
+++ api/test/controllers/TestNotificationController.ts
@@ -131,7 +131,7 @@ describe('Notifications', async () => {
       const res = await testHelper.commonUserPostRequest(teacher, '', newNotification);
       res.status.should.be.equal(200);
 
-      should.equal(await Notification.count({changedCourse: course}), course.students.length, 'Notification count mismatch');
+      should.equal(await Notification.countDocuments({changedCourse: course}), course.students.length, 'Notification count mismatch');
     });
 
     it('should not create notifications when the course is inactive', async () => {
@@ -140,7 +140,7 @@ describe('Notifications', async () => {
       const res = await testHelper.commonUserPostRequest(teacher, '', newNotification);
       res.status.should.be.equal(200);
 
-      should.equal(await Notification.count({changedCourse: course}), 0, 'Notification count mismatch');
+      should.equal(await Notification.countDocuments({changedCourse: course}), 0, 'Notification count mismatch');
     });
 
     it('should forbid notification creation for an unauthorized teacher', async () => {
@@ -172,7 +172,7 @@ describe('Notifications', async () => {
       const res = await testHelper.commonUserPostRequest(teacher, `/user/${student._id}`, newNotification);
       res.status.should.be.equal(200);
 
-      should.equal(await Notification.count({changedCourse: course}), expectedCount, 'Notification count mismatch');
+      should.equal(await Notification.countDocuments({changedCourse: course}), expectedCount, 'Notification count mismatch');
     }
 
     it('should fail if required parameters are omitted', async () => {
@@ -286,7 +286,7 @@ describe('Notifications', async () => {
       const res = await testHelper.commonUserPostRequest(teacher, `/user/${student._id}`, newNotification);
       res.status.should.be.equal(200);
 
-      should.equal(await Notification.count({user: student._id}), 1, 'Notification count mismatch');
+      should.equal(await Notification.countDocuments({user: student._id}), 1, 'Notification count mismatch');
     });
 
     it('should respond with 400 for requesting text only targetType without text', async () => {
